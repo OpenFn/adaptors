9e8858ca97bc248bb156e62286a7504aa63ee1a6
Joe Clark <josephjclark@gmail.com>
2022-10-11 18:12:14 +0100

    use esno instead of tsm as it doens't break --help

--- tools/build/src/cli.ts
+++ tools/build/src/cli.ts
@@ -1,4 +1,4 @@
-#!./node_modules/.bin/tsm
+#!./node_modules/.bin/esno
 
 // The shebang above does work, but it requires every package to have
 // tsm installed as a dev dependency
@@ -9,9 +9,12 @@ import run from './pipeline';
 
 export const cmd = yargs(hideBin(process.argv))
   .command('$0 <lang> [tasks]', 'build an adaptor')
+  .example('$0 http', 'build everything for http')
+  .example('$0 http src ast', 'run src and ast builds for http')
   .demand('lang')
   .option('tasks', {
     array: true,
+    description: 'src, ast, docs, dts',
   })
   .parse();
 
