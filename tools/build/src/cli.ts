1781994ae00d6a27d2e6aa6acf0ab7dac05ed8fc
Joe Clark <josephjclark@gmail.com>
2022-10-11 17:38:36 +0100

    Expose a bin script from build tools and use it from an adaptor

--- tools/build/src/cli.ts
+++ tools/build/src/cli.ts
@@ -1,10 +1,15 @@
+#!./node_modules/.bin/tsm
+
+// The shebang above does work, but it requires every package to have
+// tsm installed as a dev dependency
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 import run from './pipeline';
 
 export const cmd = yargs(hideBin(process.argv))
-  .command('run <lang> [tasks]', 'build an adaptor')
+  .command('$0 <lang> [tasks]', 'build an adaptor')
+  .demand('lang')
   .option('tasks', {
     array: true,
   })
