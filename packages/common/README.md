eb0952e336e10e5ecd67bd0d154677045cc2930b
mtuchi <mtuchidev@gmail.com>
2022-10-25 11:35:00 +0300

    cloned language-common into monorepo

---
+++ packages/common/README.md
@@ -0,0 +1,14 @@
+Language Common [![CircleCI](https://circleci.com/gh/OpenFn/language-common.svg?style=svg)](https://circleci.com/gh/OpenFn/language-common)
+===============
+
+Common and generic expressions and operations for [OpenFn](http://openfn.org).
+
+Development
+-----------
+
+Clone the repo, run `npm install`.
+
+Run tests using `npm run test` or `npm run test:watch`
+
+Build the project using `make`.
+
---
+++ packages/common/test/dateFns.test.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { add } from 'date-fns';
+import { dateFns } from '../';
+
+describe('date fns', () => {
+  it('should parse a random given date', () => {
+    const badString1 = '11:31AM Oct 23-2019';
+    const formatter1 = 'hh:mmaa MMM dd-yyyy';
+
+    const parsedDate = dateFns.parse(badString1, formatter1, new Date());
+
+    const offset = parsedDate.getTimezoneOffset();
+    const target = add(parsedDate, { minutes: -offset });
+
+    expect(target.toISOString()).equal('2019-10-23T11:31:00.000Z');
+  });
+
+  it('should format a date with a given format', () => {
+    const badString1 = '11:31AM Oct 23-2019';
+    const formatter1 = 'hh:mmaa MMM dd-yyyy';
+
+    const parsedDate = dateFns.parse(badString1, formatter1, new Date());
+
+    const formattedDate1 = dateFns.format(parsedDate, 'dd-MM-yyyy');
+    const formattedDate2 = dateFns.format(parsedDate, 'MM-dd-yyyy');
+    expect(formattedDate1).equal('23-10-2019');
+    expect(formattedDate2).equal('10-23-2019');
+  });
+});
