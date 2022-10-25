fee607e8cfedf3f477b68bc7527c917081c047d0
Elias W. BA <eliaswalyba@gmail.com>
2022-10-25 13:35:42 +0000

    Migrate fhir

---
+++ packages/fhir/src/Adaptor.js
@@ -0,0 +1,133 @@
+/** @module Adaptor */
+import {
+  execute as commonExecute,
+  composeNextState,
+  expandReferences,
+  http,
+} from '@openfn/language-common';
+
+const { axios } = http;
+export { axios };
+
+/**
+ * Execute a sequence of operations.
+ * Wraps `language-common/execute`, and prepends initial state for http.
+ * @example
+ * execute(
+ *   create('foo'),
+ *   delete('bar')
+ * )(state)
+ * @function
+ * @param {Operations} operations - Operations to be performed.
+ * @returns {Operation}
+ */
+export function execute(...operations) {
+  const initialState = {
+    references: [],
+    data: null,
+  };
+
+  return state => {
+    return commonExecute(...operations)({
+      ...initialState,
+      ...state,
+    });
+  };
+}
+
+/**
+ * Creates a resource in a destination system using a POST request
+ * @public
+ * @example
+ * create("/endpoint", {"foo": "bar"})
+ * @function
+ * @param {string} path - Path to resource
+ * @param {object} params - data to create the new resource
+ * @param {function} callback - (Optional) callback function
+ * @returns {Operation}
+ */
+export function create(path, params, callback) {
+  return state => {
+    path = expandReferences(path)(state);
+    params = expandReferences(params)(state);
+
+    const { baseUrl, username, password } = state.configuration;
+
+    const url = `${baseUrl}/${path}`;
+    const auth = { username, password };
+
+    const config = {
+      url,
+      body: params,
+    };
+
+    return http
+      .post(config)(state)
+      .then(response => {
+        const nextState = {
+          ...composeNextState(state, response.data),
+          response,
+        };
+        if (callback) return callback(nextState);
+        return nextState;
+      });
+  };
+}
+
+/**
+ * Creates a transactionBundle for HAPI FHIR
+ * @public
+ * @example
+ * createTransactionBundle( {"entry": [{...},, {...}]})
+ * @function
+ * @param {object} params - data to create the new transaction
+ * @param {function} callback - (Optional) callback function
+ * @returns {Operation}
+ */
+export function createTransactionBundle(params, callback) {
+  return state => {
+    params = expandReferences(params)(state);
+
+    const { resource, authType, token } = state.configuration;
+
+    const url = resource;
+    const auth = `${authType} ${token}`;
+
+    const config = {
+      url,
+      body: {
+        resourceType: 'Bundle',
+        type: 'transaction',
+        entry: params,
+      },
+      auth,
+    };
+
+    return http
+      .post(config)(state)
+      .then(response => {
+        const nextState = {
+          ...composeNextState(state, response.data),
+          response,
+        };
+        if (callback) return callback(nextState);
+        return nextState;
+      });
+  };
+}
+
+// What functions do you want from the common adaptor?
+export {
+  alterState,
+  dataPath,
+  dataValue,
+  dateFns,
+  each,
+  field,
+  fields,
+  fn,
+  http,
+  lastReferenceValue,
+  merge,
+  sourceValue,
+} from '@openfn/language-common';
