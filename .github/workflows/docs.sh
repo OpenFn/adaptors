0ef4e8291608459d498bae367e8c2a4d4310e026
mtuchi <mtuchidev@gmail.com>
2022-11-01 19:13:13 +0300

    feat: update action commit

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -2,14 +2,16 @@
 # clean docs directory
 rm -rf docs/*.md
 
-# copy new docs files 
+# copy new packages docs files to the root docs directory
 rsync -pr packages/*/docs/ docs/
 
-# Check in changes.
+# Identity config
 git config user.email "$GH_EMAIL"
 git config user.name "$GH_USER"
 
+# Commit new changes to docs
+git checkout docs
 git add docs --force
 git status
 git commit -m "Update auto-generated documentation."
-git push --set-upstream origin docs
+git push origin docs
