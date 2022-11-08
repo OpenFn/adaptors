1d60718af9ca580be8176cad45593fcf25bf371d
Joe Clark <josephjclark@gmail.com>
2022-11-08 12:01:26 +0000

    import-tests: fix tests

--- tools/import-tests/test/import-esm.mjs
+++ tools/import-tests/test/import-esm.mjs
@@ -1,7 +1,7 @@
 import { strict as assert } from 'node:assert';
 import { fn } from '@openfn/language-common';
 
-it('Should import common as a CJS module with import', () => {
+it('Should import common as an ESM module with import', () => {
   const result = fn(() => 42)();
   
   assert(result === 42);
--- tools/import-tests/test/import-json-esm.mjs
+++ tools/import-tests/test/import-json-esm.mjs
@@ -1,6 +1,6 @@
 import { strict as assert } from 'node:assert';
-import pkg from '@openfn/language-common/package.json';
+import pkg from '@openfn/language-common/package.json' assert { type: 'json' };
 
-it('Should import common as a CJS module with import', () => {
+it('Should import common/package.json as an ES module with import', () => {
   assert(pkg.name == "@openfn/language-common");
 });
