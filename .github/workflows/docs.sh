318449f2b4d3c61cadbef44e6e4b4033fb5d39ef
Taylor Downs <downs.taylor@gmail.com>
2022-11-08 15:56:40 +0000

    don't set config for more than single commit

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
-# Identity config
-git config user.email "$GH_EMAIL"
-git config user.name "$GH_USER"
 
 # sudo apt install jq
 # Commit new changes to docs
@@ -26,29 +23,28 @@ rsync -zvr packages/ tmp/ --include="*.md" --include="*/" --exclude="*" --prune-
 
 # move index.md to root directory
 find tmp/*/docs -name '*.md' -type f -execdir mv -n '{}' ../ \;
-find  tmp/*/* -type d -empty -delete;
+find tmp/*/* -type d -empty -delete
 
 # flatten json files
 # jq -s 'flatten' packages/*/docs/*.json > docs/docs.json
 rm tmp/*.json
 # add first opening bracked
-echo [ >> tmp/tmp.json
+echo [ >>tmp/tmp.json
 # use all json files in current folder
-for i in packages/*/docs/*.json
-do 
+for i in packages/*/docs/*.json; do
     # first create the key; it is the filename without the extension
     # echo \"$i\": | sed 's/\.json//' >> output/tmp.json
     # dump the file's content
-    cat "$i" >> tmp/tmp.json
+    cat "$i" >>tmp/tmp.json
     # add a comma afterwards
-    echo , >>  tmp/tmp.json
+    echo , >>tmp/tmp.json
 done
 # remove the last comma from the file; otherwise it's not valid json
-cat tmp/tmp.json | sed '$ s/.$//' >> tmp/docs.json
+cat tmp/tmp.json | sed '$ s/.$//' >>tmp/docs.json
 # remove tempfile
 rm tmp/tmp.json
 # add closing bracket
-echo ] >> tmp/docs.json
+echo ] >>tmp/docs.json
 
 # commit new docs
 git fetch origin docs
@@ -58,5 +54,5 @@ rsync -pr tmp/* docs/
 
 git add docs --force
 git status
-git commit -m "Update auto-generated documentation."
+git commit -c "user.email $GH_EMAIL" -c "user.name $GH_USER" -m "Update auto-generated documentation."
 git push origin docs
