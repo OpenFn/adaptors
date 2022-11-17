25e92a5552b64e0606b4f21c36a4179a344cca16
mtuchi <mtuchidev@gmail.com>
2022-11-17 16:29:17 +0300

    fix: index exports

--- packages/mogli/src/index.js
+++ packages/mogli/src/index.js
@@ -1,14 +1,7 @@
-// import * as Adaptor from './Adaptor';
-// import * as FakeAdaptor from './FakeAdaptor';
-
-// export default Adaptor;
-// export { FakeAdaptor, Adaptor };
-
 import * as Adaptor from './Adaptor';
-import * as FakeAdaptor from './FakeAdaptor';
 export default Adaptor;
 
+import * as FakeAdaptor from './FakeAdaptor';
 export { FakeAdaptor };
 
 export * from './Adaptor';
-export * from './FakeAdaptor';
