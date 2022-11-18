b1d2904ee3e5d828923401704ebdcf2bd68a6ce0
mtuchi <mtuchidev@gmail.com>
2022-11-18 16:15:59 +0300

    build smpp and fix test

--- packages/smpp/src/Adaptor.js
+++ packages/smpp/src/Adaptor.js
@@ -1,12 +1,14 @@
-import { execute as commonExecute, expandReferences } from 'language-common';
+import {
+  execute as commonExecute,
+  expandReferences,
+} from '@openfn/language-common';
 import request from 'request';
-import { resolve as resolveUrl } from 'url';
 
 /** @module Adaptor */
 
 /**
  * Execute a sequence of operations.
- * Wraps `language-common/execute`, and prepends initial state for http.
+ * Wraps `@openfn/language-common/execute`, and prepends initial state for http.
  * @example
  * execute(
  *   create('foo'),
@@ -19,61 +21,76 @@ import { resolve as resolveUrl } from 'url';
 export function execute(...operations) {
   const initialState = {
     references: [],
-    data: null
-  }
+    data: null,
+  };
 
   return state => {
-    return commonExecute(...operations)({ ...initialState, ...state })
+    return commonExecute(...operations)({ ...initialState, ...state });
   };
 }
 
 export function send(params) {
-
   return state => {
     const { recipient, sender, text, smsId } = expandReferences(params)(state);
 
     const { systemId, password, clientHost, inboxId } = state.configuration;
 
-    const url = `${clientHost}/smpp/${inboxId}/send`
+    const url = `${clientHost}/smpp/${inboxId}/send`;
 
-    const body = { recipient, sender, text, smsId }
+    const body = { recipient, sender, text, smsId };
 
     const auth = {
-      'username': systemId,
-      'password': password
+      username: systemId,
+      password: password,
     };
 
     function assembleError({ response, error }) {
-      if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
+      if (response && [200, 201, 202, 204].indexOf(response.statusCode) > -1)
+        return false;
       if (error) return error;
-      return new Error(`Server responded with ${response.statusCode}`)
+      return new Error(`Server responded with ${response.statusCode}`);
     }
 
-    console.log("Sending message... " + url);
-    console.log("With body: " + JSON.stringify(body, null, 2));
+    console.log('Sending message... ' + url);
+    console.log('With body: ' + JSON.stringify(body, null, 2));
 
     return new Promise((resolve, reject) => {
-      request.post ({
-        url: url,
-        json: body,
-        auth
-      }, function(error, response, body){
-        error = assembleError({response, error})
-        if(error) {
-          reject(error);
-        } else {
-          console.log("Message sent to SMPP server: " + response.statusCode + "/" + response.statusMessage);
-          resolve(response);
+      request.post(
+        {
+          url: url,
+          json: body,
+          auth,
+        },
+        function (error, response, body) {
+          error = assembleError({ response, error });
+          if (error) {
+            reject(error);
+          } else {
+            console.log(
+              'Message sent to SMPP server: ' +
+                response.statusCode +
+                '/' +
+                response.statusMessage
+            );
+            resolve(response);
+          }
         }
-      })
-    }).then((data) => {
+      );
+    }).then(data => {
       const nextState = { ...state, response: { body: data } };
       return nextState;
-    })
+    });
   };
-};
+}
 
 export {
-  field, fields, sourceValue, alterState, each,
-  merge, dataPath, dataValue, lastReferenceValue
-} from 'language-common';
+  field,
+  fields,
+  sourceValue,
+  alterState,
+  each,
+  merge,
+  dataPath,
+  dataValue,
+  lastReferenceValue,
+} from '@openfn/language-common';
