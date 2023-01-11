0dd2673e2a4609ca549967955e5328f132b7f126
mtuchi <mtuchidev@gmail.com>
2023-01-11 11:33:02 +0300

    feat: add getDeploymentInfo function

--- packages/kobotoolbox/src/Adaptor.js
+++ packages/kobotoolbox/src/Adaptor.js
@@ -112,6 +112,46 @@ export function getSubmissions(params, callback) {
   };
 }
 
+/**
+ * Get deployment information for a specific form
+ * @example
+ * getDeploymentInfo({formId: 'aXecHjmbATuF6iGFmvBLBX'}, state => {
+ *   console.log(state.data);
+ *   return state;
+ * });
+ * @function
+ * @param {object} params - Form Id and data to make the fetch or filter
+ * @param {function} callback - (Optional) Callback function to execute after fetching form deployment information
+ * @returns {Operation}
+ */
+export function getDeploymentInfo(params, callback) {
+  return state => {
+    params = expandReferences(params)(state);
+
+    const { baseURL, apiVersion, username, password } = state.configuration;
+    const { formId } = params;
+
+    const url = `${baseURL}/api/${apiVersion}/assets/${formId}/deployment/?format=json`;
+    const auth = { username, password };
+
+    const config = {
+      url,
+      params: params.query,
+      auth,
+    };
+
+    return http
+      .get(config)(state)
+      .then(response => {
+        console.log('✓', 'deployment information fetched.');
+
+        const nextState = composeNextState(state, response.data);
+        if (callback) return callback(nextState);
+        return nextState;
+      });
+  };
+}
+
 export {
   fn,
   alterState,
