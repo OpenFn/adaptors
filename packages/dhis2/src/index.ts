8af367f044b82119c5c33e29855bb2b39ee8413a
Joe Clark <josephjclark@gmail.com>
2023-01-26 14:19:22 +0000

    dhis2: remove global wrapper on types

--- packages/dhis2/src/index.ts
+++ packages/dhis2/src/index.ts
@@ -2,41 +2,39 @@
  * new rule: the dts builder will build from index.ts if it exists
  * (tsc doesnt seem to work if we build from a .d.ts)
  */
-declare global {
-  export type Dhis2Attribute = {
-    /**
-     * The attribute id
-     * @lookup $.attributes[*]
-     */
-    attribute: string;
+export type Dhis2Attribute = {
+  /**
+   * The attribute id
+   * @lookup $.attributes[*]
+   */
+  attribute: string;
 
-    value: any;
-  };
+  value: any;
+};
 
-  export type Dhis2Data = {
-    /**
-     * The id of an organisation unit
-     * @lookup $.orgUnits[*]
-     */
-    orgUnit?: string;
+export type Dhis2Data = {
+  /**
+   * The id of an organisation unit
+   * @lookup $.orgUnits[*]
+   */
+  orgUnit?: string;
 
-    /**
-     * Tracked instance id
-     */
-    trackedEntityInstance?: string;
+  /**
+   * Tracked instance id
+   */
+  trackedEntityInstance?: string;
 
-    /**
-     * Tracked instance type
-     * @lookup $.trackedEntityTypes[*]
-     */
-    trackedEntityType?: string;
+  /**
+   * Tracked instance type
+   * @lookup $.trackedEntityTypes[*]
+   */
+  trackedEntityType?: string;
 
-    /**
-     * List of attributes
-     */
-    attributes?: Dhis2Attribute[];
-  };
-}
+  /**
+   * List of attributes
+   */
+  attributes?: Dhis2Attribute[];
+};
 
 // apparently we can't just export * from index.js, we have to re-export. Bit of a bummer.
 import * as Adaptor from './Adaptor';
