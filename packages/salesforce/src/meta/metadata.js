d0b65539ef0d79ad856bafe5ce399695a17912c2
Joe Clark <jclark@openfn.org>
2023-03-08 10:57:43 +0000

    salesforce: fix crazy promise

--- packages/salesforce/src/meta/metadata.js
+++ packages/salesforce/src/meta/metadata.js
@@ -7,41 +7,36 @@ const metadata = async (configuration = {}, mock = false) => {
     helper = createMock(helper);
   }
 
-  // get the globals
   const globals = await helper.getGlobals();
 
   const root = createEntity('salesforce', 'model');
 
+  const buildModel = async ({ name, custom }) => {
+    const props = {};
+    if (!custom) {
+      props.system = true;
+    }
+    const e = createEntity(name, 'sobject', props);
+
+    const fields = await helper.getFields(name);
+    if (fields) {
+      fields.forEach(({ name, type, label, externalId }) => {
+        const f = createEntity(name, 'field', {
+          datatype: type,
+          label,
+          externalId,
+        });
+        e.addChild(f);
+
+        // TODO Now the field's attributes?
+      });
+
+      root.addChild(e);
+    }
+  };
+
   // build a model of each global sobject
-  await Promise.all(
-    globals.map(
-      ({ name, custom }) =>
-        new Promise(async resolve => {
-          const props = {};
-          if (!custom) {
-            props.system = true;
-          }
-          const e = createEntity(name, 'sobject', props);
-
-          const fields = await helper.getFields(name);
-          if (fields) {
-            fields.forEach(({ name, type, label, externalId }) => {
-              const f = createEntity(name, 'field', {
-                datatype: type,
-                label,
-                externalId,
-              });
-              e.addChild(f);
-
-              // Now the field's attributes
-            });
-
-            root.addChild(e);
-          }
-          resolve();
-        })
-    )
-  );
+  await Promise.all(globals.map(buildModel));
 
   return root;
 };
