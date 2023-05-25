ff3f8456226e3b27c5cb281da10b2fdb45d89eb2
mtuchi <mtuchidev@gmail.com>
2023-05-25 20:07:09 +0300

    cleanup

--- packages/openmrs/src/Adaptor.js
+++ packages/openmrs/src/Adaptor.js
@@ -71,14 +71,12 @@ function cleanupState(state) {
 /**
  * Gets patient matching a uuid
  * @example
- * execute(
- *   getPatient("123")
- * )(state)
+ * getPatient("123")
  * @function
  * @param {object} params - object with uuid for the patient
  * @returns {Operation}
  */
-export function getPatient(uuid, options) {
+export function getPatient(uuid) {
   return state => {
     console.log(`Searching for patient with uuid: ${uuid}`);
     const { agent } = state;
@@ -108,139 +106,51 @@ export function getPatient(uuid, options) {
 }
 
 /**
- * Gets patients matching criteria
- * @example
- * execute(
- *   getPatients(criteria)
- * )(state)
- * @function
- * @param {object} criteria - Criteria object for the patient
- * @param {object} options - Options
- * @returns {Operation}
- */
-export function getPatients(criteria, options) {
-  return state => {
-    const qs = expandReferences(criteria)(state);
-    const { instanceUrl } = state.configuration;
-    const url = resolveUrl(instanceUrl + '/', 'ws/rest/v1/patient');
-
-    console.log(`Searching for patients: ${JSON.stringify(qs, null, 2)}`);
-
-    return new Promise((resolve, reject) => {
-      request(
-        { method: 'GET', url, qs, jar: true },
-        (error, response, body) => {
-          error = assembleError({ error, response });
-          if (error) {
-            reject(error);
-          } else {
-            const data = tryJson(body);
-            const count = data.results.length;
-
-            if (options.exactlyOne && data.results.length !== 1) {
-              reject(`Raising an error because ${count} records were found.`);
-            } else {
-              console.log(
-                `Search successful. Returned ${count} patient${
-                  count > 1 ? 's' : ''
-                }.`
-              );
-            }
-
-            const nextState = composeNextState(
-              state,
-              options.exactlyOne ? data.results[0] : data.results
-            );
-            resolve(nextState);
-          }
-        }
-      );
-    });
-  };
-}
-
-/**
- * Gets people matching criteria
- * @example
- * execute(
- *   getPeople(
- *   { identifier: '007' },
- *   { exactlyOne: true }
- * )(state)
- * @function
- * @param {object} criteria - Criteria object for the people
- * @param {object} options - Options object for the handling of responses
- * @returns {Operation}
- */
-export function getPeople(criteria, options) {
-  return state => {
-    const qs = expandReferences(criteria)(state);
-    const { instanceUrl } = state.configuration;
-    const url = resolveUrl(instanceUrl + '/', 'ws/rest/v1/person');
-
-    console.log(`Searching for people: ${JSON.stringify(qs, null, 2)}`);
-
-    return new Promise((resolve, reject) => {
-      request(
-        { method: 'GET', url, qs, jar: true },
-        (error, response, body) => {
-          error = assembleError({ error, response });
-          if (error) {
-            reject(error);
-          } else {
-            const data = tryJson(body);
-            const count = data.results.length;
-
-            if (options.exactlyOne && data.results.length !== 1) {
-              reject(`Raising an error because ${count} records were found.`);
-            } else {
-              console.log(`Search successful. Returned ${count} people.`);
-            }
-
-            const nextState = composeNextState(
-              state,
-              options.exactlyOne ? data.results[0] : data.results
-            );
-            resolve(nextState);
-          }
-        }
-      );
-    });
-  };
-}
-
-/**
  * Creates an encounter
  * @example
- * execute(
- *   createEncounter(params)(state)
+ * createEncounter({
+ *   encounterDatetime: '2023-05-25T06:08:25.000+0000',
+ *   patient: '1fdaa696-e759-4a7d-a066-f1ae557c151b',
+ *   encounterType: 'dd528487-82a5-4082-9c72-ed246bd49591',
+ *   location: 'ba685651-ed3b-4e63-9b35-78893060758a',
+ *   encounterProviders: [],
+ *   visit: {
+ *     patient: '1fdaa696-e759-4a7d-a066-f1ae557c151b',
+ *     visitType: '7b0f5697-27e3-40c4-8bae-f4049abfb4ed',
+ *     startDatetime: '2023-05-25T06:08:25.000+0000',
+ *     stopDatetime: '2023-05-25T06:09:25.000+0000',
+ *   },
+ * })
  * @function
  * @param {object} params - parameters of the encounter
  * @returns {Operation}
  */
 export function createEncounter(params) {
   return state => {
-    const body = expandReferences(params)(state);
+    const body = JSON.stringify(expandReferences(params)(state));
+
+    const { agent } = state;
     const { instanceUrl } = state.configuration;
     const url = `${instanceUrl}/ws/rest/v1/encounter`;
 
     console.log(`Creating an encounter.`);
-    console.log(body);
+
     return new Promise((resolve, reject) => {
-      request(
-        { method: 'POST', url, json: body, jar: true },
-        (error, response, body) => {
-          error = assembleError({ error, response });
-          if (error) {
-            reject(error);
-          } else {
-            const data = tryJson(body);
-            console.log(`Creation successful.`);
-            const nextState = composeNextState(state, data);
-            resolve(nextState);
-          }
-        }
-      );
+      agent
+        .post(url)
+        .type('json')
+        .send(body)
+        .then(response => {
+          console.log(`Successfull created an encounter.`);
+
+          const data = tryJson(response.text);
+          const nextState = composeNextState(state, data);
+          resolve(nextState);
+        })
+        .catch(({ response }) => {
+          const { error } = response;
+          reject(error);
+        });
     });
   };
 }
@@ -251,7 +161,7 @@ export function createEncounter(params) {
  * req({
  *   method: 'GET'
  *   url: 'encounterType'
- * })(state)
+ * })
  * @function
  * @param {object} params - parameters for the request
  * @param {function} callback - a callback to execute on the next state
@@ -281,9 +191,7 @@ export function req(params, callback) {
 /**
  * Fetch all non-retired patients that match any specified parameters
  * @example
- * execute(
- *   searchPatient({ q: Sarah })
- * )(state)
+ * searchPatient({ q: Sarah })
  * @function
  * @param {object} params - object with query for the patient
  * @returns {Operation}
@@ -293,7 +201,6 @@ export function searchPatient(params) {
     const qs = expandReferences(params)(state);
     console.log(`Searching for patient with name: ${qs.q}`);
     const { agent } = state;
-
     const { instanceUrl } = state.configuration;
 
     const url = `${instanceUrl}/ws/rest/v1/patient`;
@@ -303,17 +210,25 @@ export function searchPatient(params) {
         .get(url)
         .accept('json')
         .query(qs)
-        .end((error, response) => {
-          error = assembleError({ error, response });
-          if (error) {
-            reject(error);
-          } else {
-            console.log(`Success. Found patient.`);
+        .then(response => {
+          const data = tryJson(response.text);
+          const count = data.results.length;
 
-            const data = tryJson(response.text);
+          if (count > 0) {
+            console.log(
+              `Search successful. Returned ${count} patient${
+                count > 1 ? 's' : ''
+              }.`
+            );
             const nextState = composeNextState(state, data);
             resolve(nextState);
+          } else {
+            reject(`Raising an error because ${count} records were found.`);
           }
+        })
+        .catch(({ response }) => {
+          const { error } = response;
+          reject(error);
         });
     });
   };
@@ -322,9 +237,7 @@ export function searchPatient(params) {
 /**
  * Fetch all non-retired persons that match any specified parameters
  * @example
- * execute(
- *   searchPerson({ q: Sarah })
- * )(state)
+ * searchPerson({ q: Sarah })
  * @function
  * @param {object} params - object with query for the person
  * @returns {Operation}
@@ -344,17 +257,25 @@ export function searchPerson(params) {
         .get(url)
         .accept('json')
         .query(qs)
-        .end((error, response) => {
-          error = assembleError({ error, response });
-          if (error) {
-            reject(error);
-          } else {
-            console.log(`Success. Found person.`);
+        .then(response => {
+          const data = tryJson(response.text);
+          const count = data.results.length;
 
-            const data = tryJson(response.text);
+          if (count > 0) {
+            console.log(
+              `Search successful. Returned ${count} person${
+                count > 1 ? 's' : ''
+              }.`
+            );
             const nextState = composeNextState(state, data);
             resolve(nextState);
+          } else {
+            reject(`Raising an error because ${count} records were found.`);
           }
+        })
+        .catch(({ response }) => {
+          const { error } = response;
+          reject(error);
         });
     });
   };
@@ -363,8 +284,38 @@ export function searchPerson(params) {
 /**
  * Creates a new patient
  * @example
- * execute(
- *   createPatient(params)(state)
+ * createPatient({
+ *   identifiers: [
+ *     {
+ *       identifier: '4023287',
+ *       identifierType: '05a29f94-c0ed-11e2-94be-8c13b969e334',
+ *       preferred: true,
+ *     },
+ *   ],
+ *   person: {
+ *     gender: 'M',
+ *     age: 42,
+ *     birthdate: '1970-01-01T00:00:00.000+0100',
+ *     birthdateEstimated: false,
+ *     dead: false,
+ *     deathDate: null,
+ *     causeOfDeath: null,
+ *     names: [
+ *       {
+ *         givenName: 'Doe',
+ *         familyName: 'John',
+ *       },
+ *     ],
+ *     addresses: [
+ *       {
+ *         address1: '30, Vivekananda Layout, Munnekolal,Marathahalli',
+ *         cityVillage: 'Bengaluru',
+ *         country: 'India',
+ *         postalCode: '560037',
+ *       },
+ *     ],
+ *   },
+ * })
  * @function
  * @param {object} params - parameters of the patient
  * @returns {Operation}
@@ -378,10 +329,10 @@ export function createPatient(params) {
 
     console.log(`Creating a patient.`);
 
-    console.log(body);
     return new Promise((resolve, reject) => {
       agent
         .post(url)
+        .type('json')
         .send(body)
         .then(response => {
           console.log(`Successfull created a new patient.`);
@@ -390,8 +341,8 @@ export function createPatient(params) {
           const nextState = composeNextState(state, data);
           resolve(nextState);
         })
-        .catch(({ status, response }) => {
-          const error = assembleError({ status, response });
+        .catch(({ response }) => {
+          const { error } = response;
           reject(error);
         });
     });
@@ -399,37 +350,71 @@ export function createPatient(params) {
 }
 
 /**
- * Upsert a patient
+ * Gets encounter matching a uuid
  * @example
- * execute(
- *   upsertPatient(params)(state)
+ * getEncounter("123")
  * @function
- * @param {object} params - parameters of the patient
+ * @param {object} uuid - object with uuid for the encounter
  * @returns {Operation}
  */
-export function upsertPatient(params) {
+export function getEncounter(uuid) {
   return state => {
-    const body = JSON.stringify(expandReferences(params)(state));
+    console.log(`Searching for encounter with uuid: ${uuid}`);
     const { agent } = state;
     const { instanceUrl } = state.configuration;
-    const url = `${instanceUrl}/ws/rest/v1/patient`;
 
-    console.log(`Upserting a patient.`);
+    const url = `${instanceUrl}/ws/rest/v1/encounter/${uuid}`;
 
-    console.log(body);
     return new Promise((resolve, reject) => {
       agent
-        .post(url)
-        .send(body)
+        .get(url)
+        .accept('json')
         .then(response => {
-          console.log(`Successfull upsert a patient.`);
+          console.log(`Successfull found an encounter.`);
 
           const data = tryJson(response.text);
           const nextState = composeNextState(state, data);
           resolve(nextState);
         })
-        .catch((error, response) => {
-          error = assembleError({ error, response });
+        .catch(({ response }) => {
+          const { error } = response;
+          reject(error);
+        });
+    });
+  };
+}
+
+/**
+ * Gets encounters matching params
+ * @example
+ * getEncounters({patient: "123", fromdate: "2023-05-18"})
+ * @function
+ * @param {object} params - Criteria object for the patient
+ * @returns {Operation}
+ */
+export function getEncounters(params) {
+  return state => {
+    const qs = expandReferences(params)(state);
+    const { agent } = state;
+    const { instanceUrl } = state.configuration;
+    const url = `${instanceUrl}/ws/rest/v1/encounter`;
+
+    console.log(`Searching for encounters: ${JSON.stringify(qs, null, 2)}`);
+
+    return new Promise((resolve, reject) => {
+      agent
+        .get(url)
+        .accept('json')
+        .query(qs)
+        .then(response => {
+          console.log(`Successfull found an encounter.`);
+
+          const data = tryJson(response.text);
+          const nextState = composeNextState(state, data);
+          resolve(nextState);
+        })
+        .catch(({ response }) => {
+          const { error } = response;
           reject(error);
         });
     });
