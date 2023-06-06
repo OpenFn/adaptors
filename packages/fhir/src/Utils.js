c9d579d8f3209ad6f12d073e46a4aad59ca31bb9
Taylor Downs <downs.taylor@gmail.com>
2023-06-06 10:46:55 +0100

    remove Log class for fhir

--- packages/fhir/src/Utils.js
+++ packages/fhir/src/Utils.js
@@ -1,23 +1,5 @@
 import { composeNextState } from '@openfn/language-common';
 
-export class Log {
-  static info(message) {
-    return console.info(`ℹ `, message);
-  }
-
-  static success(message) {
-    return console.info(`✓ Success at ${new Date()}:\n∟`, message);
-  }
-
-  static warn(message) {
-    return console.warn(`⚠ Warning at ${new Date()}:\n∟`, message);
-  }
-
-  static error(message) {
-    return console.error(`✗ Error at ${new Date()}:\n∟`, message);
-  }
-}
-
 export function handleResponse(response, state, callback) {
   const nextState = {
     ...composeNextState(state, response.data),
