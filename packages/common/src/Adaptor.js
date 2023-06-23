1092cdd1b4a954d6a0444c929005f92870f5b528
Emmanuel Evance <mtuchidev@gmail.com>
2023-06-23 13:14:07 +0300

    improve callback docs

--- packages/common/src/Adaptor.js
+++ packages/common/src/Adaptor.js
@@ -559,7 +559,7 @@ export function chunk(array, chunkSize) {
  *     - `rtrim` {boolean} - Ignore whitespace characters from the right side of a CSV field. Default: `true`
  *     - `chunkSize` {number} - The size of each chunk of CSV data. Default: `Infinity`
  *     - `skip_empty_lines` {boolean} - The `skip_empty_lines` skips any line which is empty. Default: `true`
- * @param {function} callback - (Optional) callback function
+ * @param {function} callback - (Optional) callback function. If used it will take state and csvRows
  * @returns {Promise} The function returns a Promise that resolves to the result of parsing a CSV `stringOrStream`.
  */
 export function parseCsv(csvData, parsingOptions = {}, callback) {
