93b03c54b4bfd04ca77a5600ecc079229a64224f
Emmanuel Evance <mtuchidev@gmail.com>
2023-09-18 12:35:17 +0300

    wip submit xls

--- packages/msgraph/src/Adaptor.js
+++ packages/msgraph/src/Adaptor.js
@@ -263,6 +263,71 @@ export function getFile(pathOrId, options, callback = s => s) {
   };
 }
 
+/**
+ * Convert form data to xls then submit.
+ * @public
+ * @example
+ * submitXls(
+ *    [
+ *      {name: 'Mamadou', phone: '000000'},
+ *    ],
+ *    {
+ *      case_type: 'student',
+ *      search_field: 'external_id',
+ *      create_new_cases: 'on',
+ *    }
+ * )
+ * @function
+ * @param {Object} formData - Object including form data.
+ * @param {Object} params - Request params including case type and external id.
+ * @returns {Operation}
+ */
+export function submitXls(request, data, callback) {
+  return state => {
+    const { accessToken, apiVersion } = state.configuration;
+
+    const [resolvedRequest, resolvedData] = expandReferences(
+      state,
+      request,
+      data
+    );
+    const { name, path } = resolvedRequest;
+
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.json_to_sheet(resolvedData);
+    const ws_name = 'SheetJS';
+    xlsx.utils.book_append_sheet(workbook, worksheet, ws_name);
+
+    // Generate buffer
+    const buffer = xlsx.write(workbook, { type: 'buffer', bookType: 'xls' });
+
+    const data = new FormData();
+
+    data.append('file', buffer, { filename: 'output.xls' });
+
+    data.append('case_type', case_type);
+    data.append('search_field', search_field);
+    data.append('create_new_cases', create_new_cases);
+
+    console.log('Posting to url: '.concat(url));
+    return http
+      .post({
+        url,
+        data,
+        headers: {
+          ...data.getHeaders(),
+          Authorization: `ApiKey ${username}:${apiKey}`,
+        },
+      })(state)
+      .then(response => {
+        return { ...state, data: { body: response.data } };
+      })
+      .catch(err => {
+        throw { ...err, config: {}, request: {} };
+      });
+  };
+}
+
 export { request } from './Utils';
 
 export {
