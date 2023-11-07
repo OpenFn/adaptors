a9309c71d5630be20c8da3dd65340b5c759b608b
Joe Clark <jclark@openfn.org>
2023-11-07 17:33:51 +0000

    common: add operation factory

--- packages/common/src/Adaptor.js
+++ packages/common/src/Adaptor.js
@@ -15,6 +15,21 @@ export * as dateFns from './dateFns';
 
 const schemaCache = {};
 
+// operation creates a new operation factory
+// It accepts a function which takes user arguments AND state
+// It returns a function which acccepts user args
+// Which in turn returns a function which accepts state
+// and then triggers the original function
+// It's complex BUT the end user code should be way way simpler
+export const operation = (fn) => {
+  return (...args) => {
+    return (state) => {
+      return fn(state, ...args)
+    }
+  }
+}
+
+
 /**
  * Execute a sequence of operations.
  * Main outer API for executing expressions.
