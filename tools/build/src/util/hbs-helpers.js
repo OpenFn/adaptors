2e6cd16eed6b70775a953d933a12b064f786664f
jclark <jclark@openfn.org>
2024-03-22 11:42:33 +0000

    tidy

--- tools/build/src/util/hbs-helpers.js
+++ tools/build/src/util/hbs-helpers.js
@@ -4,18 +4,6 @@ exports.toLowerCase = function (str) {
   return str ? str.toLowerCase() : str;
 };
 
-// const common = () => {
-//   return options.data.root.filter(function (o) {
-//     console.log(o)
-//     return true;
-//   //   if (identifier.kind === 'external') {
-//   //     return identifier.description && identifier.description.length > 0
-//   //   } else {
-//   //     return true
-//   //   }
-//   // })
-// }
-
 exports.commonFns = function(options) {
   options.hash.scope = 'global'
   const common = () => handlebars.helpers._identifiers(options).filter((o) => {
