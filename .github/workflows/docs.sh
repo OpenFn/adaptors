4cd7fa0b9628b9801d73d4bd4f329c8860f97396
jclark <jclark@openfn.org>
2024-03-22 11:41:21 +0000

    revert script changes

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -35,12 +35,12 @@ rm tmp/tmp.json
 echo ] >>tmp/docs.json
 
 # commit new docs
-# git fetch origin docs
-# git switch docs
+git fetch origin docs
+git switch docs
 
 rsync -pr tmp/* docs/
 
-# git add docs --force
-# git status
-# git add -A && git commit -m"Update auto-generated documentation."
-# git push origin docs
+git add docs --force
+git status
+git add -A && git commit -m"Update auto-generated documentation."
+git push origin docs
