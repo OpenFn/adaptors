b1e152b50c410331a4ebf4182d872a7605933adc
Hunter Achieng <achienghunter@gmail.com>
2024-05-27 16:51:33 +0300

    fix:remove assert from package
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/satusehat/src/Utils.js
+++ packages/satusehat/src/Utils.js
@@ -3,7 +3,7 @@ import {
   request as commonRequest,
   logResponse,
 } from '@openfn/language-common/util';
-import pkg from '../package.json' assert { type: 'json' };
+import pkg from '../package.json';
 
 export const authorize = state => {
   const auth = state.configuration;
