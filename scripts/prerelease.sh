8dc828319bf757ffcefceb9847f85becbe4b4af1
jclark <jclark@openfn.org>
2024-06-13 16:47:22 +0100

    allow a dirty tree

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -13,7 +13,7 @@ if test -f .changeset/pre.json; then
   pnpm changeset version
 
   # publish
-  pnpm publish -r --report-summary --publish-branch main --access=public
+  pnpm publish -r --report-summary --publish-branch main --access=public --no-git-checks
 else
   echo "No prelease detected. Skipping release"
 
