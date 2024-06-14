a7a6e7d00d9e11e9bca2ede96710679c4b66ab40
jclark <jclark@openfn.org>
2024-06-14 15:48:02 +0100

    tools: update prerelease script

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -1,16 +1,15 @@
 # if there's a pre file
+# TODO maybe grep pre.json for `"mode": "pre"`
 if test -f .changeset/pre.json; then
   echo "Publishing pre-release build"
 
-  # TODO I'd really like a robust way to tell if we're in pre mode
-  # I suppose this works
-  # will this reset numbers or anything?
-  # the other option is to use jq to read the json
-  # so this is not safe to do when already in pre mode
-  #pnpm changeset pre enter next
-
   # bump versions
   pnpm changeset version
+
+  ## check changes into git
+  git add .
+  git commit -m "Bump pre-release versions"
+  git push origin `git rev-parse --abbrev-ref HEAD`
 
   # publish
   pnpm publish -r --report-summary --publish-branch main --access=public --no-git-checks --tag next
