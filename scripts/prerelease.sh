2c64653c9403a3b78eba231686db90d11b128305
jclark <jclark@openfn.org>
2024-06-14 16:26:39 +0100

    stuff

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -7,10 +7,12 @@ if test -f .changeset/pre.json; then
   pnpm changeset version
 
   ## check changes into git
+  git switch -c $GITHUB_HEAD_REF
+  
   git add .
+  
   git commit -m "Bump pre-release versions"
-  git switch -c `$GITHUB_HEAD_REF`
-  git push origin `$GITHUB_HEAD_REF`
+  #git push origin $GITHUB_HEAD_REF
 
   # publish
   # pnpm publish -r --report-summary --publish-branch main --access=public --no-git-checks --tag next
