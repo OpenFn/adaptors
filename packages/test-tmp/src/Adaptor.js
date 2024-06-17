86ec3d44458a68eccd77a2f825736a8a065b26a8
jclark <jclark@openfn.org>
2024-06-17 14:28:41 +0100

    test-tmp: fix json import

--- packages/test-tmp/src/Adaptor.js
+++ packages/test-tmp/src/Adaptor.js
@@ -1,9 +1,10 @@
 import { expandReferences } from '@openfn/language-common/util';
 import * as util from './Utils';
 
-import pkg from '../package.json' assert { type: 'json' };
-
 export function version() {
+  const pkgRaw = require('../package.json');
+  const pkg = JSON.parse(pkgRaw);
+
   return state => ({
     ...state,
     version: pkg.version,
