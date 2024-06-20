fbdff61f83be801a5045ca7200952e5ccb6fe715
jclark <jclark@openfn.org>
2024-06-20 10:10:36 +0100

    add new prerelease script
    This uses way more automation and doesn't create a remote commit

---
+++ scripts/prerelease.mjs
@@ -0,0 +1,73 @@
+import util from 'node:util';
+import c_p from 'node:child_process';
+import path from 'node:path';
+import { readFile, writeFile } from 'node:fs/promises';
+import summary from '../pnpm-publish-summary.json' assert { type: 'json' };
+
+const exec = util.promisify(c_p.exec);
+
+async function updatePackage(packageName, suffix) {
+  const name = packageName.substr('@openfn/langauge-'.length);
+
+  const pkgPath = path.resolve(`packages/${name}/package.json`);
+  let pkg = await readFile(pkgPath, 'utf8');
+  const pkgJson = JSON.parse(pkg);
+  pkgJson.version = `${pkgJson.version}-next+${suffix}`;
+
+  console.log(`Updated version of ${name} to ${pkgJson.version}`);
+
+  writeFile(pkgPath, JSON.stringify(pkgJson, null, 2));
+}
+
+async function prerelease() {
+  console.log('Generating changesets');
+
+  // Run changeset version to process all the active
+  // changesets and get the net version numbes
+  await exec('pnpm changeset version');
+
+  console.log(`generating pnpm report`);
+  // Publish with dry-run just to get the report
+  await exec(
+    `pnpm publish \
+      -r \
+      --report-summary \
+      --publish-branch main \
+      --no-git-checks \
+      --dry-run`
+  );
+
+  console.log('Updating versions');
+
+  // For each package in the report,
+  // update the version to include the commit hash
+  const hash = process.env.GITHUB_SHA?.substring(0, 8) ?? 'local'; // TOOD get from gh env
+
+  const { default: report } = await import('../pnpm-publish-summary.json', {
+    assert: { type: 'json' },
+  });
+
+  for (const pkg of report.publishedPackages) {
+    console.log(pkg);
+    updatePackage(pkg.name, hash);
+  }
+
+  await exec('pnpm install');
+  await exec('pnpm build');
+
+  console.log('Publishing to npm');
+
+  // TODO I may need a commit here to allow the publish
+
+  await exec(`pnpm publish \
+    -r \
+    --report-summary \
+    --publish-branch main \
+    --access=public \
+    --no-git-checks \
+    --tag next`);
+
+  console.log('done!');
+}
+
+prerelease();
