671de7b43a3c64acb6e59bac6a11aecc678ef635
jclark <jclark@openfn.org>
2024-06-28 12:36:43 +0100

    tools: update template
    -use base url
    -fix headers and options handling
    -include RequestOptions type def
    -include state docs

--- tools/generate/template/src/Adaptor.js
+++ tools/generate/template/src/Adaptor.js
@@ -2,18 +2,41 @@ import { expandReferences } from '@openfn/language-common/util';
 import * as util from './Utils';
 
 /**
+ * State object
+ * @typedef {Object} HttpState
+ * @private
+ * @property data - the parsed response body
+ * @property response - the response from the HTTP server, including headers, statusCode, body, etc
+ * @property references - an array of all previous data objects used in the Job
+ **/
+
+/**
+ * Options provided to the HTTP request
+ * @typedef {Object} RequestOptions
+ * @property {object|string} body - body data to append to the request. JSON will be converted to a string (but a content-type header will not be attached to the request).
+ * @property {object} errors - Map of errorCodes -> error messages, ie, `{ 404: 'Resource not found;' }`. Pass `false` to suppress errors for this code.
+ * @property {object} form - Pass a JSON object to be serialised into a multipart HTML form (as FormData) in the body.
+ * @property {object} query - An object of query parameters to be encoded into the URL.
+ * @property {object} headers - An object of headers to append to the request.
+ * @property {string} parseAs - Parse the response body as json, text or stream. By default will use the response headers.
+ * @property {number} timeout - Request timeout in ms. Default: 300 seconds.
+ * @property {object} tls - TLS/SSL authentication options. See https://nodejs.org/api/tls.html#tlscreatesecurecontextoptions
+ */
+
+/**
  * Make a GET request
  * @example
  * get("patient");
  * @function
  * @public
  * @param {string} path - Path to resource
- * @param {Object} params - Optional request params
+ * @param {RequestOptions} options - Optional request options
  * @param {function} [callback] - Optional callback to handle the response
  * @returns {Operation}
+ * @state {HttpState}
  */
-export function get(path, params, callback) {
-  return request('GET', path, null, params, callback);
+export function get(path, options, callback) {
+  return request('GET', path, null, options, callback);
 }
 
 /**
@@ -24,12 +47,13 @@ export function get(path, params, callback) {
  * @public
  * @param {string} path - Path to resource
  * @param {object} data - Object which will be attached to the POST body
- * @param {Object} params - Optional request params
+ * @param {RequestOptions} options - Optional request options
  * @param {function} [callback] - Optional callback to handle the response
  * @returns {Operation}
+ * @state {HttpState}
  */
-export function post(path, params, callback) {
-  return request('POST', path, null, params, callback);
+export function post(path, options, callback) {
+  return request('POST', path, null, options, callback);
 }
 
 /**
@@ -41,22 +65,23 @@ export function post(path, params, callback) {
  * @param {string} method - HTTP method to use
  * @param {string} path - Path to resource
  * @param {object} data - Object which will be attached to the POST body
- * @param {Object} params - Optional request params
+ * @param {RequestOptions} options - Optional request options
  * @param {function} [callback] - Optional callback to handle the response
  * @returns {Operation}
+ * @state {HttpState}
  */
-export function request(method, path, body, params = {}, callback = s => s) {
+export function request(method, path, body, options = {}, callback = s => s) {
   return async state => {
-    const [resolvedMethod, resolvedPath, resolvedData, resolvedParams] =
-      expandReferences(state, method, path, body, params);
+    const [resolvedMethod, resolvedPath, resolvedData, resolvedoptions] =
+      expandReferences(state, method, path, body, options);
 
     const response = await util.request(
       state.configuration,
       resolvedMethod,
       resolvedPath,
       {
-        params: resolvedParams,
         data: resolvedData,
+        ...resolvedoptions,
       }
     );
 
