a1d60c9193114fcd53abf30e0b91f3b7da2004c4
jclark <jclark@openfn.org>
2024-07-24 16:09:49 +0100

    common: fix exports

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,6 +1,8 @@
 import * as Adaptor from './Adaptor';
 export default Adaptor;
 
+export * from './Adaptor';
+
 export * as beta from './beta';
 export * as http from './http';
 export * as dateFns from './dateFns';
