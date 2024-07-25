d3030099aab5b2cf168620b4a9d8c9d37f37c149
jclark <jclark@openfn.org>
2024-07-25 17:23:53 +0100

    common: fix typings

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,5 +3,9 @@ export default Adaptor;
 
 export * from './Adaptor';
 
+export * as beta from './beta';
+export * as http from './http';
+export * as dateFns from './dateFns';
+
 import * as metadata from './metadata';
 export { metadata };
