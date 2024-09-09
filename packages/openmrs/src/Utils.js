8823610b1c0e653d0e9ce45000673dfa15d1dedb
Emmanuel Evance <mtuchidev@gmail.com>
2024-09-09 13:56:06 +0300

    add autoFetchRequest helper function

--- packages/openmrs/src/Utils.js
+++ packages/openmrs/src/Utils.js
@@ -2,11 +2,11 @@ import { composeNextState } from '@openfn/language-common';
 import {
   request as commonRequest,
   makeBasicAuthHeader,
-  logResponse
+  logResponse,
 } from '@openfn/language-common/util';
 
 export const prepareNextState = (state, response, callback) => {
-  const { body, ...responseWithoutBody } = response
+  const { body, ...responseWithoutBody } = response;
   const nextState = {
     ...composeNextState(state, response.body),
     response: responseWithoutBody,
@@ -21,18 +21,52 @@ export function request(state, method, path, data, params) {
 
   const options = {
     body: data,
-
     headers: {
       ...headers,
       'content-type': 'application/json',
     },
-
     query: params,
-
     parseAs: 'json',
   };
 
   const url = `${instanceUrl}${path}`;
 
-  return commonRequest(method, url, options).then(response => logResponse(response));
+  return autoFetchRequest(method, url, options);
 }
+
+const autoFetchRequest = async (method, url, options) => {
+  let allResponses;
+  let reqQuery = options?.query;
+  let allowPagination = isNaN(reqQuery?.startIndex);
+
+  do {
+    const response = await commonRequest(method, url, {
+      ...options,
+      query: reqQuery ?? {},
+    });
+    logResponse(response);
+
+    allResponses
+      ? allResponses.body.results.push(...response.body.results)
+      : (allResponses = response);
+
+    const nextUrl = response?.body?.links?.find(
+      link => link.rel === 'next'
+    )?.uri;
+
+    if (nextUrl) {
+      console.log(`Found ${response.body.results.length} results`);
+      console.log(`Fetching next page from ${nextUrl}`);
+      const urlObj = new URL(nextUrl);
+      const params = new URLSearchParams(urlObj.search);
+      const startIndex = params.get('startIndex');
+
+      reqQuery = { ...reqQuery, startIndex };
+    } else {
+      delete allResponses.body.links;
+      break;
+    }
+  } while (allowPagination);
+
+  return allResponses;
+};
