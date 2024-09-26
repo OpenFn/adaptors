ece269d066ae8953f8b9b569a30641640d4ec48c
Joe Clark <jclark@openfn.org>
2024-09-26 19:25:52 +0100

    tidying

--- jembi.js
+++ jembi.js
@@ -1,17 +1,18 @@
 import '@openfn/language-common';
 // // we should have type defs on this
-import { create, builders, b, util } from '@openfn/language-fhir-jembi';
+import { create, builders, b, util } from '@openfn/language-fhir-ndr-et';
 
 // and this should actually run
 // console.log(builders);
 const encounter = b.encounter({
   id: 'wibble',
   identifier: 'identify me baby',
+  
 });
 
 console.log(encounter);
 
-create('Patient', { id: 'big willy smith' })();
+create('Patient', { id: 'big willy smith',  })();
 
 console.log('hello');
 
--- packages/fhir-ndr-et/src/Adaptor.js
+++ packages/fhir-ndr-et/src/Adaptor.js
@@ -1,17 +1,12 @@
-// TODO temporily disabled because I've broken the fhir build
 import { expandReferences } from '@openfn/language-common/util';
-// import { util } from '@openfn/language-fhir';
 
 // This is only available at after build
 // @ts-ignore
 import * as builders from './builders';
-// import { getBuilderName } from '../build/util'; // hmm
 
 export const getBuilderName = resourceName =>
   resourceName[0].toLowerCase() + resourceName.slice(1);
 
-// TODO this is a lightweight wrapper around fhir create which uses the builder function
-// and typings
 /**
  * Creates a new resource with a server assigned resourceType.
  * The resource object doesn't need resourceType or id
@@ -54,22 +49,19 @@ export const create = (resourceType, resource, params, callback = s => s) => {
       body: r,
     };
 
-    // const response = await util.request(
-    //   state.configuration,
-    //   'POST',
-    //   resolvedResourceType,
-    //   opts
-    // );
-    // return util.prepareNextState(state, response, callback);
+    const response = await util.request(
+      state.configuration,
+      'POST',
+      resolvedResourceType,
+      opts
+    );
+    return util.prepareNextState(state, response, callback);
   };
 };
-
-// TOOD this is causing me build issues - work out how to restore this later
 
 export {
   dataPath,
   dataValue,
-  //   // dateFns,
   cursor,
   each,
   field,
