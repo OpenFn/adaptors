38620b313742230a6bbfb8bf80c0ce56e6a6d712
Emmanuel Evance <mtuchidev@gmail.com>
2024-10-01 23:01:29 +0300

    wip: add fhirGet helper

--- packages/openmrs/src/Utils.js
+++ packages/openmrs/src/Utils.js
@@ -65,3 +65,51 @@ export async function request(state, method, path, data, params) {
 
   return allResponses;
 }
+
+export async function fhirRequest(state, method, path, data, params) {
+  const { instanceUrl, username, password } = state.configuration;
+  const headers = makeBasicAuthHeader(username, password);
+
+  const options = {
+    body: data,
+    headers: {
+      ...headers,
+      'content-type': 'application/json',
+    },
+    query: params,
+    parseAs: 'json',
+  };
+
+  const url = `${instanceUrl}${path}`;
+
+  let allResponses;
+  let query = options?.query;
+  let allowPagination = isNaN(query?._getpagesoffset);
+
+  do {
+    const requestOptions = query ? { ...options, query } : options;
+    const response = await commonRequest(method, url, requestOptions);
+    logResponse(response);
+
+    if (allResponses) {
+      allResponses.body.entry.push(...response.body.entry);
+    } else {
+      allResponses = response;
+    }
+    const nextUrl = response?.body?.link?.find(l => l.relation === 'next')?.url;
+    if (nextUrl) {
+      console.log(`Fetched ${response.body.entry.length} results`);
+      console.log(`Fetching next page from ${nextUrl}`);
+      const urlObj = new URL(nextUrl);
+      const params = new URLSearchParams(urlObj.search);
+      const _getpagesoffset = params.get('_getpagesoffset');
+
+      query = { ...query, _getpagesoffset };
+    } else {
+      delete allResponses.body.link;
+      break;
+    }
+  } while (allowPagination);
+
+  return allResponses;
+}
