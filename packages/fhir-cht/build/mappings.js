600b74136c6bb5fd7b50340e85a9119eb7e7eb62
Joe Clark <joe@openfn.org>
2024-11-25 13:03:07 +0000

    fhir-cht: update

--- packages/fhir-cht/build/mappings.js
+++ packages/fhir-cht/build/mappings.js
@@ -1,1 +1,6 @@
-export default {};
+export default {
+  include: ['Patient'],
+
+  // TODO because a spec has many many value sets, and the my be large
+  // we should probably list the valuesets to include here
+};
