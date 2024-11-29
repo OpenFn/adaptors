7787d7c5c4096aa6f3e034f3c07b02e903391033
Joe Clark <joe@openfn.org>
2024-11-29 15:57:00 +0000

    don't ignore types

---
+++ packages/fhir-fr/types/builders.d.ts
@@ -0,0 +1,136 @@
+/**
+  * Create a FHIR Address resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function address(type: string, props: any): any;
+/**
+  * Create a FHIR Appointment resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function appointment(type: string, props: any): any;
+/**
+  * Create a FHIR ContactPoint resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function contactPoint(type: string, props: any): any;
+/**
+  * Create a FHIR Encounter resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function encounter(type: string, props: any): any;
+/**
+  * Create a FHIR Extension resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function extension(type: string, props: any): any;
+/**
+  * Create a FHIR HealthcareService resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function healthcareService(type: string, props: any): any;
+/**
+  * Create a FHIR HumanName resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function humanName(type: string, props: any): any;
+/**
+  * Create a FHIR Location resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function location(type: string, props: any): any;
+/**
+  * Create a FHIR MedicationAdministration resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function medicationAdministration(type: string, props: any): any;
+/**
+  * Create a FHIR Observation resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function observation(type: string, props: any): any;
+/**
+  * Create a FHIR Organization resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function organization(type: string, props: any): any;
+/**
+  * Create a FHIR Patient resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function patient(type: string, props: any): any;
+/**
+  * Create a FHIR Practitioner resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function practitioner(type: string, props: any): any;
+/**
+  * Create a FHIR PractitionerRole resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function practitionerRole(type: string, props: any): any;
+/**
+  * Create a FHIR RelatedPerson resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function relatedPerson(type: string, props: any): any;
+/**
+  * Create a FHIR Schedule resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function schedule(type: string, props: any): any;
+/**
+  * Create a FHIR Slot resource.
+  * @public
+  * @function
+  * @param {string} type - The profile id for the resource variant
+  * @param props - Properties to apply to the resource
+ */
+export function slot(type: string, props: any): any;
