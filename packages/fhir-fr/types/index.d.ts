819115f84711a5be0b3fda7ecd703c7011199c5c
Joe Clark <joe@openfn.org>
2024-12-01 15:21:35 +0000

    fhir-fr: build

--- packages/fhir-fr/types/index.d.ts
+++ packages/fhir-fr/types/index.d.ts
@@ -1,4 +1,6 @@
 import './builders';
+import * as Adaptor from './Adaptor';
+export default Adaptor;
 export * as util from './utils';
 export * as builders from './builders';
 export * as b from './builders';
