cd851b69c515cdc9627132316b02ec9b22f5538c
Emmanuel Evance <mtuchidev@gmail.com>
2025-01-13 16:32:10 +0300

    test asyncMode

--- packages/salesforce/test/integration.js
+++ packages/salesforce/test/integration.js
@@ -17,6 +17,13 @@ describe('Integration tests', () => {
     before(async () => {
       state.data = [{ name: 'Coco', vera__Active__c: 'No' }];
     });
+    it.only('should return batchInfo if asyncMode: false', async () => {
+      const { data } = await execute(
+        bulk('Account', 'insert', state => state.data, { asyncMode: false })
+      )(state);
+      console.log(data);
+      // expect(data.batchInfo).to.be.an('object');
+    }).timeout(10000);
     it('should create multiple sobjects', async () => {
       const { data } = await execute(
         bulk('Account', 'insert', state => state.data)
