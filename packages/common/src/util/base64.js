557a7f532bff289b555d450a900e35626dfc0620
pius <piuskariuki19@gmail.com>
2025-01-14 22:46:16 +0300

    Changed the signatures of the decode and encode functions to include an options object that allows users to explicitly disable the JSON parse step

--- packages/common/src/util/base64.js
+++ packages/common/src/util/base64.js
@@ -1,9 +1,12 @@
+import _ from 'lodash';
+
 /**
  * Encodes a given string or Javascript object into Base64 format.
  * @function
  * @public
  * @namespace util
  * @param {string | object} data - The string or object to be encoded.
+ * @param {object} options - Options.
  * @returns {string} - The Base64 encoded string.
  * @example <caption>Encode a string</caption>
  * const encodedString = encode('Hello World');
@@ -11,12 +14,13 @@
  * @example <caption>Encode an object</caption>
  * const encodedObject = encode({name: 'Jane Doe'})
  * console.log(encodedObject); //output eyJuYW1lIjoiSmFuZSBEb2UifQ==
- *
+ * @example <caption>To skip the JSON stringification step</caption>
+ * const encodedObject = encode('Hello World', {parseJson: false})
  */
-export const encode = data => {
+export const encode = (data, options = {parseJson: true}) => {
   let str = data;
 
-  if(typeof data !== "string"){
+  if(typeof data !== "string" && options.parseJson){
     try {
       str = JSON.stringify(str);
     } catch (e){
@@ -33,18 +37,25 @@ export const encode = data => {
  * @public
  * @namespace util
  * @param {string} base64Data - The Base64 encoded string.
+ * @param {object} options - Options.
  * @returns {string | object} - The decoded string or JavaScript Object.
  * @example <caption>Decode a Base64 string</caption>
  * const decoded = decode('SGVsbG8gV29ybGQ=');
  * @example <caption>Decode a Base64 JSON object to a standard JavaScript object</caption>
  * const decoded = decode('eyJuYW1lIjoiSmFuZSBEb2UifQ==');
  * console.log(decoded); // Output: {name: 'Jane Doe'}
+ * @example <caption>To skip the JSON stringification step</caption>
+ * const decodedString = decode('Hello World', {parseJson: false})
  */
-export const decode = base64Data =>{
+export const decode = (base64Data, options = {parseJson: true}) =>{
   let decodedObject = Buffer.from(base64Data, 'base64').toString('utf-8');
 
-  if(['[', '{'].includes(decodedObject[0])){
-    decodedObject = JSON.parse(decodedObject);
+  if((_.startsWith(decodedObject, '[') || _.startsWith(decodedObject, '{')) && options.parseJson) {
+    try {
+      decodedObject = JSON.parse(decodedObject);
+    } catch (e) {
+      console.log(e.message);
+    }
   }
 
   return decodedObject;
