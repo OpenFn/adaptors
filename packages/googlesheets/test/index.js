506442256b0ec87ab75ff37a1e677a41b3144097
pius <piuskariuki19@gmail.com>
2025-01-18 11:46:14 +0300

    Added unit tests to check of the functions return early in the event of an empty of nullish value

--- packages/googlesheets/test/index.js
+++ packages/googlesheets/test/index.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import { execute } from '../src';
+import { appendValues, batchUpdateValues } from '../src';
 
 describe('execute', () => {
   it('executes each operation in sequence', done => {
@@ -34,5 +35,36 @@ describe('execute', () => {
         data: null,
       });
     });
+  });
+});
+
+
+describe('append', () =>{
+  it('should  return early if the values array is undefined or nullish', async() => {
+    const state = {
+      data: [],
+    }
+
+    const result = await appendValues({
+      spreadsheetId: '123-456-789',
+      range: 'Sheet!A1:E1',
+      values: state.values,
+    })(state);
+    expect(result).to.eql(state);
+  });
+});
+
+describe('batchUpdateValues', () =>{
+  it('should  return early if the values array is undefined or nullish', async() => {
+    const state = {
+      data: [],
+    }
+
+    const result = await batchUpdateValues({
+      spreadsheetId: '123-456-789',
+      range: 'Sheet!A1:E1',
+      values: state.values,
+    })(state);
+    expect(result).to.eql(state);
   });
 });
