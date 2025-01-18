12b37a2fca8e15fe8f6557d1485e0640955b08de
Hunter Achieng <achienghunter@gmail.com>
2025-01-18 16:43:43 +0300

    fix: update docs
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/openmrs/src/http.js
+++ packages/openmrs/src/http.js
@@ -4,19 +4,19 @@ import * as util from './Utils';
 /**
  * Make a HTTP request to any OpenMRS endpoint
  * @example
- * request("GET","/ws/rest/v1/patient/d3f7e1a8-0114-4de6-914b-41a11fc8a1a8", {
- *   {query: {
- *   q: "Patient",
- *   limit: 1,
- *   },
- *  baseUrl: "http://msf-ocg-openmrs3-dev.westeurope.cloudapp.azure.com/openmrs"
- * }
+ * request("GET",
+ *   "/ws/rest/v1/patient/d3f7e1a8-0114-4de6-914b-41a11fc8a1a8", {
+ *    query:{ 
+ *       limit: 1, 
+ *       offset: 20 
+ *    },
+ *     baseUrl: "http://msf-ocg-openmrs3-dev.westeurope.cloudapp.azure.com/openmrs"
  * });
  * @function
  * @public
  * @param {string} method - HTTP method to use
  * @param {string} path - Path to resource
- * @param {import('./Utils').OpenMRSOptions}  options - An object containing either query, headers, and body for the request
+ * @param {import('./Utils').OpenMRSOptions}  [options={}] - An object containing either query, headers, and body for the request
  * @returns {Operation}
  */
 export function request(method, path, options = {}, callback = s => s) {
