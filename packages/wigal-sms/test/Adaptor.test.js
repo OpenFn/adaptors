264a923ff91ef89bb6affffec67ecb818ee6abfa
Emmanuel Evance <mtuchidev@gmail.com>
2025-01-27 10:36:23 +0300

    clean up

--- packages/wigal-sms/test/Adaptor.test.js
+++ packages/wigal-sms/test/Adaptor.test.js
@@ -1,31 +1,30 @@
 import { expect } from 'chai';
 
-import {execute, sendSms } from '../src/Adaptor.js';
+import { execute, sendSms } from '../src/Adaptor.js';
 
 describe('sendSMS', () => {
   it('Send SMS via Wigal', async () => {
     const state = {
       configuration: {
-       /* baseUrl :'https://frogapi.wigal.com.gh',
-        apiKey:'sampleapikey',
-        username :'Stevkky'*/
-    },
+        // baseUrl: 'https://frogapi.wigal.com.gh',
+        // apiKey: 'sampleapikey',
+        // username: 'Stevkky',
+      },
       data: {
-        senderid : 'Stevkky',
+        senderid: 'Stevkky',
         destinations: [
           {
-            destination: "0552825710",
+            destination: '0552825710',
           },
         ],
-        message: "This is a sample message for SMS sending via Wigal FROG API.",
-        smstype: "text"
+        message: 'This is a sample message for SMS sending via Wigal FROG API.',
+        smstype: 'text',
       },
     };
 
-    const finalState = await execute(
-      sendSms(state => state.data)
-    )(state);
+    const { data } = await execute(sendSms(state => state.data))(state);
 
-   // console.log(finalState.data);
+    expect(data.status).to.eql('ACCEPTED');
+    expect(data.message).to.eql('Message Accepted For Processing');
   });
 });
