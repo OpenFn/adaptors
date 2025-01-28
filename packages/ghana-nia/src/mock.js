f634a1063f160bc363e591d575b57067401919fb
Taylor Downs <downs.taylor@gmail.com>
2025-01-28 14:40:05 +0000

    add mock validator to NIA

--- packages/ghana-nia/src/mock.js
+++ packages/ghana-nia/src/mock.js
@@ -1,9 +1,42 @@
 import { MockAgent } from 'undici';
+import { validateRequestBody } from '../../../tools/mock-validator.js';
 
-const niaResponseSample = {
+const sampleRequestBody = {
+  merchantKey: '89487284-9083-4015-9128-91d8db7e023e',
+  babyData: {
+    dateOfBirth: '2024-03-05',
+    fatherName: 'Nyarkoa Osei-Akoto',
+    forenames: 'Kharis',
+    gender: 'Female',
+    lightwaveETrackerID: '00313180/24-03',
+    motherName: 'Gifty Osei-Akoto',
+    noSiblingsInDelivery: '0',
+    placeOfBirth: 'New Market Health Centre',
+    surname: 'Osei',
+    timeOfbirth: '09:34',
+    weightAtBirth: '2.7',
+    birthCertificateNumber: '011803-48-2024',
+    babyPicture: '...base64 encoded image goes here...',
+  },
+  personVouching: {
+    etrackerLightwaveID: '00313180/24-03',
+    ghanaCardPIN: 'GHA-001097272-4',
+    relationToBaby: 'Mother',
+    relativePhone: '0248403076',
+    relativePicture: '...base64 encoded image goes here...',
+  },
+};
+
+const nationalIdSampleResponse = {
   data: {
-    babyPin: 'GH-6326223626-3'
-  }
+    babyPin: 'GHA-000000000-1',
+    voucherPin: 'GHA-000000000-4',
+    etrackerLightwaveId: '00000000/24-03',
+    lightwaveEtrackerId: null,
+  },
+  success: true,
+  code: '00',
+  msg: 'Saved Successfully',
 };
 
 // This creates a mock NIA server
@@ -14,13 +47,30 @@ export function createServer(url = 'https://selfie.imsgh.org:2035') {
   const mockPool = agent.get(url);
 
   const sendNiaData = req => {
-    return {
-      statusCode: 200,
-      responseOptions: {
-        headers: { 'Content-Type': 'application/json' },
-      },
-      data: JSON.stringify(niaResponseSample),
-    };
+    if (validateRequestBody(JSON.parse(req.body), sampleRequestBody)) {
+      return {
+        statusCode: 200,
+        responseOptions: {
+          headers: { 'Content-Type': 'application/json' },
+        },
+        data: JSON.stringify(nationalIdSampleResponse),
+      };
+    } else {
+      return {
+        // Why is this a 404 coming from NIA?
+        statusCode: 404,
+        responseOptions: {
+          headers: { 'Content-Type': 'application/json' },
+        },
+        data: {
+          success: false,
+          code: '05',
+          data: null,
+          // sic
+          msg: 'Error in Verifiation Process',
+        },
+      };
+    }
   };
 
   mockPool
--- packages/ghana-nia/test/Adaptor.test.js
+++ packages/ghana-nia/test/Adaptor.test.js
@@ -21,16 +21,14 @@ describe('registerChild', () => {
           timeOfbirth: '10:25am',
           weightAtBirth: '3kg',
           birthCertificateNumber: '2323423423',
-          babyPicture:
-            'iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=',
+          babyPicture: 'iVBORw0KGgoAAAANSUh...',
         },
         personVouching: {
           etrackerLightwaveID: 'IJggqTs7hxL',
           ghanaCardPIN: 'GHS-236723623-7',
           relationToBaby: 'Mother',
           relativePhone: '026252672',
-          relativePicture:
-            'iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAANCSURBVEiJtZZPbBtFFMZ/M7ubXdtdb1xSFyeilBapySVU8h8OoFaooFSqiihIVIpQBKci6KEg9Q6H9kovIHoCIVQJJCKE1ENFjnAgcaSGC6rEnxBwA04Tx43t2FnvDAfjkNibxgHxnWb2e/u992bee7tCa00YFsffekFY+nUzFtjW0LrvjRXrCDIAaPLlW0nHL0SsZtVoaF98mLrx3pdhOqLtYPHChahZcYYO7KvPFxvRl5XPp1sN3adWiD1ZAqD6XYK1b/dvE5IWryTt2udLFedwc1+9kLp+vbbpoDh+6TklxBeAi9TL0taeWpdmZzQDry0AcO+jQ12RyohqqoYoo8RDwJrU+qXkjWtfi8Xxt58BdQuwQs9qC/afLwCw8tnQbqYAPsgxE1S6F3EAIXux2oQFKm0ihMsOF71dHYx+f3NND68ghCu1YIoePPQN1pGRABkJ6Bus96CutRZMydTl+TvuiRW1m3n0eDl0vRPcEysqdXn+jsQPsrHMquGeXEaY4Yk4wxWcY5V/9scqOMOVUFthatyTy8QyqwZ+kDURKoMWxNKr2EeqVKcTNOajqKoBgOE28U4tdQl5p5bwCw7BWquaZSzAPlwjlithJtp3pTImSqQRrb2Z8PHGigD4RZuNX6JYj6wj7O4TFLbCO/Mn/m8R+h6rYSUb3ekokRY6f/YukArN979jcW+V/S8g0eT/N3VN3kTqWbQ428m9/8k0P/1aIhF36PccEl6EhOcAUCrXKZXXWS3XKd2vc/TRBG9O5ELC17MmWubD2nKhUKZa26Ba2+D3P+4/MNCFwg59oWVeYhkzgN/JDR8deKBoD7Y+ljEjGZ0sosXVTvbc6RHirr2reNy1OXd6pJsQ+gqjk8VWFYmHrwBzW/n+uMPFiRwHB2I7ih8ciHFxIkd/3Omk5tCDV1t+2nNu5sxxpDFNx+huNhVT3/zMDz8usXC3ddaHBj1GHj/As08fwTS7Kt1HBTmyN29vdwAw+/wbwLVOJ3uAD1wi/dUH7Qei66PfyuRj4Ik9is+hglfbkbfR3cnZm7chlUWLdwmprtCohX4HUtlOcQjLYCu+fzGJH2QRKvP3UNz8bWk1qMxjGTOMThZ3kvgLI5AzFfo379UAAAAASUVORK5CYII=',
+          relativePicture: 'iVBORw0KGgoAAAA...',
         },
       },
     };
@@ -39,8 +37,14 @@ describe('registerChild', () => {
 
     expect(finalState.data).to.deep.equal({
       data: {
-        babyPin: 'GH-6326223626-3',
+        babyPin: 'GHA-000000000-1',
+        voucherPin: 'GHA-000000000-4',
+        etrackerLightwaveId: '00000000/24-03',
+        lightwaveEtrackerId: null,
       },
+      success: true,
+      code: '00',
+      msg: 'Saved Successfully',
     });
   });
 });
