a55893431603df756506c9787e10e08291c5ff6b
Taylor Downs <downs.taylor@gmail.com>
2025-01-28 14:20:27 +0000

    bdr updates

--- packages/ghana-bdr/src/mock.js
+++ packages/ghana-bdr/src/mock.js
@@ -1,11 +1,10 @@
 import { MockAgent } from 'undici';
 
-const birthNotificationResponse = {
-  registry_code: '1',
+const sampleRequestBody = {
   child: {
     first_name: 'Nana',
     middle_name: 'Nana',
-    surname: 'Kumasi',
+    Surname: 'Kumasi',
     birth_date: '2023/10/22',
     national_id_number: '90282901892',
     place_of_delivery_code: '2',
@@ -14,8 +13,8 @@ const birthNotificationResponse = {
     gender_code: '2',
     attendant_at_birth_code: '1',
     attendant_at_birth_other: null,
-    birth_attendant_full_name: 'Dr Lawrence Kwame',
-    birth_attendant_phone: '0556969609',
+    bith_attendant_full_name: 'Dr Lawrence Kwame',
+    bith_attendant_phone: '0556969609',
   },
   mother: {
     first_name: 'Nana',
@@ -65,14 +64,42 @@ const birthNotificationResponse = {
     district_code: '11',
     region_code: '12',
   },
-  birth_informant: {
-    relationship_code: '2',
-    relationship_other: null,
-    full_name: 'Kufor Mensa',
-    phone: '055646874',
-    digital_address: 'No. 64 Ajiringanor',
-    national_id_number: '993993939-0303',
-  },
+};
+
+const birthNotificationResponse = {
+  birth_certificate_number: '000000-00-2024',
+  first_name: 'Test',
+  middle_name: '',
+  Surname: 'sample surname',
+  birth_date: '04 Mar 2024',
+  national_id_number: '',
+  place_of_delivery: 'MATERNITY HOME',
+  Health_facility: '',
+  birth_type: null,
+  gender: 'MALE',
+  house_number: '',
+  street_name: '',
+  district: '',
+  region: '',
+  town_or_village: '',
+  m_first_name: 'sample mom',
+  m_middle_name: '',
+  m_maiden_name: 'sample surname',
+  m_surname: '',
+  m_national_id_number: 'GHA-000000000-0',
+  m_nationality: '',
+  f_first_name: 'father fname',
+  f_middle_name: '',
+  f_Surname: 'father sur',
+  f_nationality: '',
+  f_national_id_number: '',
+  reference_id: 'abc123de-1995',
+  document_number: '1995/000000/XX0000000A1B2C3',
+  created_at: '05 Mar 2024',
+  last_updated_at: null,
+  issuccessful: true,
+  message: 'record reference_id : abc123de-1995 , created successfully',
+  messagecode: '200',
 };
 
 // This creates a mock bdr server
@@ -83,14 +110,58 @@ export function createServer(url = 'http://tracker.chimgh.org') {
 
   const mockPool = agent.get(url);
 
+  const validateRequestBody = (request, sample) => {
+    // Check top-level keys
+    const sampleKeys = Object.keys(sample);
+    const requestKeys = Object.keys(request);
+
+    for (const key of sampleKeys) {
+      // make sure all keys are present
+      if (!requestKeys.includes(key)) {
+        console.log('Key missing:', key);
+        return false;
+      }
+
+      // make sure all the value of all keys have the same value type
+      if (typeof sample[key] != typeof request[key]) {
+        console.log('Key with wrong value type:', key);
+        return false;
+      }
+
+      // If the value is an object, recursively validate
+      // can we check the keys of each key (assuming it's an object)
+      if (typeof sample[key] === 'object' && sample[key] !== null) {
+        if (!validateRequestBody(request[key], sample[key])) {
+          console.log('Failed check at:', key);
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
   const sendBirthNotification = req => {
-    return {
-      statusCode: 200,
-      responseOptions: {
-        headers: { 'Content-Type': 'application/json' },
-      },
-      data: JSON.stringify(birthNotificationResponse),
-    };
+    if (validateRequestBody(JSON.parse(req.body), sampleRequestBody)) {
+      return {
+        statusCode: 200,
+        responseOptions: {
+          headers: { 'Content-Type': 'application/json' },
+        },
+        data: JSON.stringify(birthNotificationResponse),
+      };
+    } else {
+      return {
+        statusCode: 417,
+        responseOptions: {
+          headers: { 'Content-Type': 'application/json' },
+        },
+        data: {
+          Message:
+            '{"issuccessful":false,"message":"Record failed to save with this error -->  Modify the clause to make sure that a column is updated only once. If this statement updates or inserts columns into a view, column aliasing can conceal the duplication in your code.","messagecode":"210"} (Note that this is just a sample error from a mock endpoint.)',
+        },
+      };
+    }
   };
 
   mockPool
