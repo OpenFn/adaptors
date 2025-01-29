2b5d11a7788c3d1f35ae47babc8c499f0c4050d9
Joe Clark <joe@openfn.org>
2025-01-29 09:43:09 +0000

    ghana-bdr: revert json schema
    Left the schema stuff commented out

--- packages/ghana-bdr/src/mock.js
+++ packages/ghana-bdr/src/mock.js
@@ -1,12 +1,79 @@
 import { MockAgent } from 'undici';
-import Ajv from 'ajv';
+import { validateRequestBody } from './util';
 
-// Generated from sample with https://www.jsongenerator.io/schema
-// But!! I had to replace $schema with $id
-// Note: use import() or ele src build breaks
-const reqSchemaString = import('./schema/request.json');
-const reqSchema = JSON.parse(reqSchemaString);
-const validate = new Ajv().compile(reqSchema);
+// import Ajv from 'ajv';
+// // Generated from sample with https://www.jsongenerator.io/schema
+// // But!! I had to replace $schema with $id
+// // Note: use import() or ele src build breaks
+// const reqSchemaString = import('./schema/request.json');
+// const reqSchema = JSON.parse(reqSchemaString);
+// const validate = new Ajv().compile(reqSchema);
+
+const sampleRequestBody = {
+  child: {
+    first_name: 'Nana',
+    middle_name: 'Nana',
+    Surname: 'Kumasi',
+    birth_date: '2023/10/22',
+    national_id_number: '90282901892',
+    place_of_delivery_code: '2',
+    place_of_delivery_other: null,
+    birth_type_code: '1',
+    gender_code: '2',
+    attendant_at_birth_code: '1',
+    attendant_at_birth_other: null,
+    bith_attendant_full_name: 'Dr Lawrence Kwame',
+    bith_attendant_phone: '0556969609',
+  },
+  mother: {
+    first_name: 'Nana',
+    middle_name: 'Nana',
+    maiden_name: 'Nana',
+    surname_or_marriage_name: 'Nana',
+    phone: '05673673673',
+    age: 35,
+    national_id_number: '90-030-003',
+    nationality_code: '131',
+    house_number: 'No 4536',
+    street_name: 'Accra city Ghana',
+    district_code: '10',
+    region_code: '10',
+    town_or_village: 'Ajiringanor',
+    number_of_children_ever_born_alive: 5,
+    born_alive_are_living: 5,
+    live_birth_order: 5,
+    born_alive_are_dead: 0,
+    education_level_code: '6',
+    education_level_other: null,
+    occupation_code: '13',
+    occupation_other: null,
+  },
+  father: {
+    first_name: 'Kufor',
+    middle_name: 'Kufor',
+    surname: 'Mensa',
+    nationality_code: '131',
+    national_id_number: '88-9292-92092',
+    phone: '05673673737',
+    age: 38,
+    education_level_code: '13',
+    occupation_code: '13',
+    religion_code: '1',
+    education_level_other: null,
+    occupation_other: null,
+    religion_other: null,
+    is_gainfully_employed_code: '1',
+  },
+  health_facility: {
+    name: 'Korlebu General Hospital',
+    house_number: 'No 4536',
+    street_name: 'Adjiringano',
+    town_code: '12',
+    town_other: null,
+    district_code: '11',
+    region_code: '12',
+  },
+};
 
 const birthNotificationResponse = {
   birth_certificate_number: '000000-00-2024',
@@ -53,7 +120,8 @@ export function createServer(url = 'http://tracker.chimgh.org') {
   const mockPool = agent.get(url);
 
   const sendBirthNotification = req => {
-    if (validate(JSON.parse(req.body))) {
+    //if (validate(JSON.parse(req.body))) {
+    if (validateRequestBody(req, sampleRequestBody)) {
       return {
         statusCode: 200,
         responseOptions: {
@@ -63,7 +131,7 @@ export function createServer(url = 'http://tracker.chimgh.org') {
         data: JSON.stringify(JSON.stringify(birthNotificationResponse)),
       };
     } else {
-      console.log('Validation errors:', validate.errors);
+      // console.log('Validation errors:', validate.errors);
       return {
         statusCode: 417,
         responseOptions: {
@@ -72,8 +140,6 @@ export function createServer(url = 'http://tracker.chimgh.org') {
         data: {
           Message:
             '{"issuccessful":false,"message":"Record failed to save with this error -->  Modify the clause to make sure that a column is updated only once. If this statement updates or inserts columns into a view, column aliasing can conceal the duplication in your code.","messagecode":"210"} (Note that this is just a sample error from a mock endpoint.)',
-          // from the mock:
-          _errors: validate.errors,
         },
       };
     }
