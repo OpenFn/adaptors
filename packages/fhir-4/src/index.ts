bbc55984412b443a0c904f234297c5d8c0392d24
Joe Clark <joe@openfn.org>
2025-01-31 09:57:12 +0000

    fhir4: ts tweaks

--- packages/fhir-4/src/index.ts
+++ packages/fhir-4/src/index.ts
@@ -3,7 +3,10 @@ import './builders';
 import * as Adaptor from './Adaptor';
 export default Adaptor;
 
-export * as util from './utils';
+// this wierd? export as dt?
+// or should we right datatypes to builders?
+// why not b.identifier?
+export * as util from './datatypes';
 
 export * as builders from './builders';
 export * as b from './builders';
