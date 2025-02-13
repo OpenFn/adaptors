b2988daeefd1dfa56bff74abe4a7e877a2157312
pius <piuskariuki19@gmail.com>
2025-02-13 22:06:34 +0300

    - Updating the tests

--- packages/openmrs/test/index.test.js
+++ packages/openmrs/test/index.test.js
@@ -507,7 +507,7 @@ describe('get', () => {
       })
       .reply(200, { uuid: '123' }, { ...jsonHeaders });
 
-    const { data } = await execute(get('/encounter/123/'))(state);
+    const { data } = await execute(get('/encounter/123'))(state);
 
     expect(data.uuid).to.eql('123');
   });
@@ -520,7 +520,7 @@ describe('get', () => {
       })
       .reply(200, { results: testData.patientResults }, { ...jsonHeaders });
 
-    const { data } = await execute(get('patient/', {query: {q: 'Sarah'}}))(state);
+    const { data } = await execute(get('patient', {query: {q: 'Sarah'}}))(state);
 
     expect(data.results[0].uuid).to.eql(testData.patientResults[0].uuid);
   });
