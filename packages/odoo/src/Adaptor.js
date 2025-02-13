5669f361500b99d8c93fe7c42788b2ccd482ba2c
Hunter Achieng <achienghunter@gmail.com>
2025-02-13 14:34:21 +0300

    feature: implement odoo adaptor
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/odoo/src/Adaptor.js
+++ packages/odoo/src/Adaptor.js
@@ -1,8 +1,6 @@
-import {
-  execute as commonExecute,
-  composeNextState,
-} from '@openfn/language-common';
+import { execute as commonExecute } from '@openfn/language-common';
 import { expandReferences } from '@openfn/language-common/util';
+import { prepareNextState } from './Utils';
 
 import pkg from 'odoo-await';
 const Odoo = pkg;
@@ -57,25 +55,116 @@ async function login(state) {
   return state;
 }
 /**
- * Create a lead in Odoo
+ * Create a record in Odoo
  * @public
  * @example
- * createLead("res.partner", { name: "Kool Keith" });
+ * create("res.partner", { name: "Kool Keith" });
  * @function
+ * @param {string} model - The specific record model i.e. "res.partner"
+ * @param {object} data - The data to be created in JSON.
+ * @param {string} externalId - Optional external ID for the record.
  * @returns {Operation}
  */
-export function createLead(model, params, externalId) {
+export function create(model, data, externalId) {
   return async state => {
-    const [resolvedModel, resolvedParams, resolvedExternalId] =
-      expandReferences(state, model, params, externalId);
+    const [resolvedModel, resolvedData, resolvedExternalId] = expandReferences(
+      state,
+      model,
+      data,
+      externalId
+    );
 
-    console.log(resolvedModel, resolvedParams, resolvedExternalId);
-    let lead = await odooConn.create(
+    console.log(resolvedModel, resolvedData, resolvedExternalId);
+    const response = await odooConn.create(
       resolvedModel,
-      resolvedParams,
+      resolvedData,
       resolvedExternalId
     );
-    console.log(lead);
+    return prepareNextState(state, response);
+  };
+}
+
+/**
+ * Get a record from Odoo
+ * @public
+ * @example
+ * read("res.partner", [1] , [name]);
+ * @function
+ * @param {string} model - The specific record model from i.e. "res.partner"
+ * @param {number[]} recordId - An array of record IDs to read.
+ * @param {string[]} fields - An array of fields to read from the record.
+ * @returns {Operation}
+ */
+export function read(model, recordId, fields) {
+  return async state => {
+    const [resolvedModel, resolvedRecordId, resolvedFields] = expandReferences(
+      state,
+      model,
+      recordId,
+      fields
+    );
+
+    console.log(resolvedModel, resolvedRecordId, resolvedFields);
+    const response = await odooConn.read(
+      resolvedModel,
+      resolvedRecordId,
+      resolvedFields
+    );
+    return prepareNextState(state, response);
+  };
+}
+
+/**
+ * Update a record in Odoo
+ * @public
+ * @example
+ * update("res.partner", 54 , {name: 'Jane Doe'});
+ * @function
+ * @param {string} model - The specific record model i.e. "res.partner"
+ * @param {number} recordId - The specific recordId to be updated.
+ * @param {object} data - The data to be updated in JSON.
+ * @returns {Operation}
+ */
+export function update(model, recordId, data) {
+  return async state => {
+    const [resolvedModel, resolvedRecordId, resolvedData] = expandReferences(
+      state,
+      model,
+      recordId,
+      data
+    );
+
+    console.log(resolvedModel, resolvedRecordId, resolvedData);
+    const response = await odooConn.update(
+      resolvedModel,
+      resolvedRecordId,
+      resolvedData
+    );
+    return prepareNextState(state, response);
+  };
+}
+
+/**
+ * Delete a record from Odoo
+ * @public
+ * @example
+ * deleteRecord("res.partner", 54 );
+ * @function
+ * @param {string} model - The specific record model i.e. "res.partner"
+ * @param {number} recordId - The specific recordId to be deleted.
+ * @returns {Operation}
+ */
+export function deleteRecord(model, recordId) {
+  return async state => {
+    const [resolvedModel, resolvedRecordId] = expandReferences(
+      state,
+      model,
+      recordId
+    );
+
+    console.log(resolvedModel, resolvedRecordId);
+    const response = await odooConn.delete(resolvedModel, resolvedRecordId);
+    return prepareNextState(state, response);
   };
 }
 
---
+++ packages/odoo/src/Utils.js
@@ -0,0 +1,14 @@
+import { composeNextState } from '@openfn/language-common';
+
+export const prepareNextState = (state, response) => {
+  const { body, ...responseWithoutBody } = response;
+
+  if (!state.references) {
+    state.references = [];
+  }
+
+  return {
+    ...composeNextState(state, response.body),
+    response: responseWithoutBody,
+  };
+};
