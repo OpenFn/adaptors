8621395c11f9312f6a7039df5b8c491838d58486
pius <piuskariuki19@gmail.com>
2025-03-06 11:24:37 +0300

    first commit

---
+++ packages/claude/CHANGELOG.md
@@ -0,0 +1,5 @@
+# @openfn/language-anthropic
+
+## 1.0.0
+
+Initial release.
---
+++ packages/claude/src/Adaptor.js
@@ -0,0 +1,72 @@
+import { composeNextState, execute as commonExecute } from '@openfn/language-common';
+import Anthropic from '@anthropic-ai/sdk';
+
+let client;
+
+/**
+ * Creates and Anthropic client
+ * @param state
+ * @returns {state}
+ */
+function createClient(state) {
+  const { apiKey } = state.configuration;
+
+  client = new Anthropic({
+    apiKey
+  });
+
+  return state;
+}
+
+/**
+ * Executes a series of operations.
+ * It ensures the client is created first and assigned to the 'client' variable
+ * @private
+ * @param operations
+ * @returns {operation}
+ */
+export function execute(...operations) {
+  const initialState = {
+    references: [],
+    data: null
+  };
+
+  return state => {
+    return commonExecute(createClient, ...operations)({
+      ...initialState,
+      ...state,
+    })
+  }
+}
+
+
+/**
+ * Write a prompt
+ * @public
+ * @param {string} message - The prompt
+ * @returns {operation}
+ */
+export function createPrompt (message) {
+  return async state => {
+    const msg = await client.messages.create({
+      model: 'claude-3-7-sonnet-20250219',
+      max_tokens: 1024,
+      messages: [{ role: 'user', content: message }]
+    });
+    return composeNextState(state, msg)
+  }
+}
+
+export {
+  dataPath,
+  dataValue,
+  dateFns,
+  cursor,
+  each,
+  field,
+  fields,
+  fn,
+  lastReferenceValue,
+  merge,
+  sourceValue,
+} from '@openfn/language-common';
