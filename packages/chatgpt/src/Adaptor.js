4942530a77d8a9a52dfc3ab95b38790d6c98e522
pius <piuskariuki19@gmail.com>
2025-03-06 13:00:25 +0300

    first commit

---
+++ packages/chatgpt/src/Adaptor.js
@@ -0,0 +1,16 @@
+import OpenAI from "openai"
+
+
+export {
+  dataPath,
+  dataValue,
+  dateFns,
+  cursor,
+  each,
+  field,
+  fields,
+  fn,
+  lastReferenceValue,
+  merge,
+  sourceValue,
+} from '@openfn/language-common';
