060ae6e844dbf00ec588e4baf7ba689c31285e0b
pius <piuskariuki19@gmail.com>
2025-03-06 13:28:08 +0300

    prompt operation

--- packages/chatgpt/src/Adaptor.js
+++ packages/chatgpt/src/Adaptor.js
@@ -1,5 +1,63 @@
-import OpenAI from "openai"
+import { composeNextState, execute as commonExecute } from '@openfn/language-common';
+import OpenAI from 'openai';
 
+let client;
+
+/**
+ * Creates and OpenAI client
+ * @param state
+ * @returns {state}
+ */
+function createClient(state) {
+  const { apiKey } = state.configuration;
+
+  client = new OpenAI({
+    apiKey
+  });
+
+  return state;
+}
+
+/**
+ * Executes a series of operations.
+ * It ensures the client is created first and assigned to the 'client' variable
+ * @private
+ * @param operations
+ * @returns {operation}
+ */
+export function execute(...operations) {
+  const initialState = {
+    references: [],
+    data: null
+  };
+
+  return state => {
+    return commonExecute(createClient, ...operations)({
+      ...initialState,
+      ...state,
+    })
+  }
+}
+
+
+/**
+ * Prompt the GPT chat interface to respond
+ * @example
+ * prompt('Write a haiku about surfing.');
+ * @public
+ * @param {string} message - The prompt
+ * @param model - The model (defaults to 'gpt-4o)
+ * @returns {operation}
+ */
+export function prompt (message, model='gpt-4o') {
+  return async state => {
+    const msg = await client.chat.completions.create({
+      model,
+      messages: [{ role: 'user', content: message }]
+    });
+    return composeNextState(state, msg)
+  }
+}
 
 export {
   dataPath,
