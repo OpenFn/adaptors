27eb31b51bf1babdfe298f1889a088a9cac28a76
Jason DeCarteret <jdecarteret@kopiustech.com>
2025-03-12 20:09:46 -0700

    Refactored key functions into utility files.

--- packages/varo/README.md
+++ packages/varo/README.md
@@ -1,31 +1,286 @@
 # language-varo <img src='./assets/square.png' width="30" height="30"/>
 
-An OpenFn **_adaptor_** for building integration jobs for use with the varo API.
+The Varo adaptor works in conjunction with the Gmail adaptor. It takes proprietary data formats found in `state.data` and attempts to convert them to EMS format.
 
-## Documentation
+Expected data structure:
 
-View the [docs site](https://docs.openfn.org/adaptors/packages/varo-docs)
-for full technical documentation.
-
-### Configuration
-
-View the
-[configuration-schema](https://docs.openfn.org/adaptors/packages/varo-configuration-schema/)
-for required and optional `configuration` properties.
-
-### A example expression using the `create` function
-
-```js
-post('patient', { name: 'Bukayo' });
+```
+{ 
+  state: {
+    data: [
+      {
+        metadata: {
+          content: '',
+          filename: '',
+        },
+      },
+      {
+        data: {
+          content: '',
+          filename: '',
+        },
+      },
+    ]
+  }
+}
 ```
 
-## Development
 
-Clone the [adaptors monorepo](https://github.com/OpenFn/adaptors). Follow the
-"Getting Started" guide inside to get set up.
+## Setup the primary workflow and jobs
 
-Run tests using `pnpm run test` or `pnpm run test:watch`
+This will demonstrate how to pull messages from a Gmail account and pass them to the Varo adaptor which will convert them into EMS format to be used for downstream consumers in the workflow.
 
-Build the project using `pnpm build`.
+The workflow requires some pre-configuration, namely the Gmail `access_token` and the OpenFn `collection_token`.
 
-To build _only_ the docs run `pnpm build docs`.
+### Token configuration
+
+#### Gmail token
+
+Use Postman to retrieve an access token. This is a short-lived token will last 60 minutes and will have to be manually retrieved. See the documentation in the [Gmail adaptor readme](https://docs.openfn.org/adaptors/packages/gmail-readme#use-the-postman-application-to-query-the-oauth-enpoint-and-retrieve-an-access-token) for a guide on how to configure Postman to retrieve the access token.
+
+#### OpenFn collections token
+
+The workflow tracks the previously processed messages by storing the processed IDs in an OpenFn collection. OpenFn requires authorization to access a given collection.
+
+1. Access the [Collections](http://localhost:4000/settings/collections) configuration in the administration area.
+1. Ensure the collection named `gmail-processed-ids` exists.
+1. Create a new token in the [Person Access Tokens](http://localhost:4000/profile/tokens) configuration.
+
+### Workflow
+
+Place these files in the openfn/adaptors/workflows folder
+
+#### workflow.js
+
+```
+{
+  "options": {
+    "start": "getContentsFromMessages"
+  },
+  "workflow": {
+    "steps": [
+      {
+        "id": "getContentsFromMessages",
+        "adaptors": [
+          "gmail",
+          "collections"
+        ],
+        "expression": "jobGmail.js",
+        "configuration": {
+          "access_token": "[redacted]"
+        },
+        "next": {
+          "getEmsData": "Array.isArray(state.data) && state.data.length > 0"
+        }
+      },
+      {
+        "id": "getEmsData",
+        "adaptor": "varo",
+        "expression": "jobVaro.js",
+      }
+    ],
+    "credentials": {
+      "collections_endpoint": "http://localhost:4000/collections",
+      "collections_token": "[redacted]"
+    }
+  }
+}
+```
+
+### Jobs
+
+#### gmail.js
+
+This job will define message parts of interest including: metadata, data and fridgeTag. Also important is a collection of previously-processed messageIds. This job will grab ids from the `gmail-processed-ids` collection and pass them into the request. Once the request is complete, the new, processed messageIds are placed into the collection for future retrieval.
+
+```
+const contents = [
+  { name: "metadata", file: /\d{12}_\d{8}T\d{6}Z\.json$/ },
+  { name: "data", file: /_CURRENT_DATA_.*\.json$/ },
+  {
+    name: "data",
+    archive: /_varo_data\.zip$/,
+    file: /_CURRENT_DATA_.*\.json$/,
+  },
+  { name: "fridgeTag", file: /\d{12}_\d{12}_\d{8}T\d{6}Z\.txt$/ },
+];
+
+const collectionName = "gmail-processed-ids";
+const itemName = "processedIds";
+
+collections.get(collectionName, itemName);
+
+getContentsFromMessages({
+  contents,
+  processedIds: $.data,
+});
+
+fnIf(
+  (state) => Array.isArray(state.processedIds),
+  collections.set(collectionName, itemName, $.processedIds)
+);
+```
+
+#### varo.js
+
+The function `getEmsData` works in conjunction with the Gmail adaptor and expects `state.data` in the format provided by the Gmail adaptor. The function accepts the incoming `state.data` which contains text files in EMS-like Varo format or FridgeTag format and transforms them into EMS-structured data.
+
+```
+getEmsData();
+
+fn((state) => {
+  console.log(state.data);
+  return state;
+});
+```
+
+### Running the workflow
+
+The `-m` flag tells OpenFn to use the monorepo instead of its own adaptor cache.
+
+```
+cd openfn/adaptors/workflows
+openfn -m workflow.js
+```
+
+## Setup the abbreviated Varo-only workflow
+
+It's beneficial to isolate the Varo adaptor during development to avoid the redundant step of repeatedly querying Gmail which also requires refreshing the access token each hour. This example shows how to use a pre-retrieved Gmail adaptor `state.data` in place of the actual Gmail adaptor.
+
+### Workflow
+
+The Gmail adaptor adds message "content", which is a collection of objects in `state.data` which contain the desired contents of the messages.
+
+We can configure the workflow to retrieve this message content from a local file which will bypass the need to use the Gmail adaptor to retrieve this information.
+
+#### workflowVaro.js
+
+```
+{
+  "options": {
+    "start": "getEmsData"
+  },
+  "workflow": {
+    "steps": [
+      {
+        "id": "getEmsData",
+        "adaptor": "varo",
+        "expression": "jobVaroOnly.js",
+        "state": "allMessages.json"
+      }
+    ],
+   }
+}
+```
+
+### Job
+
+#### varo.js
+
+Due to a quirk when injecting data into the state, we can't set the `state.data` property directly. Instead, we have to pull the messages from `state.messages` and manually assign it to `state.data` to simulate a payload that was delivered directly from the Gmail adaptor.
+
+```
+fn((state) => {
+  state.data = state.messages;
+  state.messages = null;
+  return state;
+});
+
+getEmsData();
+
+fn((state) => {
+  console.log(state.data);
+  return state;
+});
+```
+
+### Running the workflow
+
+The `-m` flag tells OpenFn to use the monorepo instead of its own adaptor cache.
+
+```
+cd openfn/adaptors/workflows
+openfn -m workflowVaro.js
+```
+
+These instructions will illustrate how to install OpenFn and the adaptor monorepo. The monorepo gives you access to in-development versions of the adaptors. Included are examples of setting up a workflow with:
+
+- jobs to utilize the Gmail adaptor and the Varo adaptor
+- a job to simulate the Gmail adaptor and to isolate the Varo adaptor
+
+Recommended folder structure:
+
+```
+openfn
+├── lightning
+└── adaptors
+    ├── adaptors
+    └── workflows
+```
+
+# Enhancing/developing the Varo adaptor
+
+## Install OpenFn
+
+Prerequisite is Docker in installed, up-to-date, and running on your computer.
+
+### Clone the Lightning repository
+
+```
+cd openfn
+git clone https://github.com/OpenFn/lightning.git
+```
+
+### Build the containers
+
+This will build three docker containers:
+
+1. lightning-web
+1. postgres
+1. ws-worker
+
+```
+docker compose build && docker compose run --rm web mix ecto.migrate
+docker compose up -d
+```
+
+The web application will be running on [localhost port 4000](http://localhost:4000).
+
+## Install the monorepo
+
+To use the monorepo locally you'll need some prerequesite build tools installed.
+
+### Prerequesites
+
+asdf, nodejs, pnpm
+
+```
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add pnpm
+```
+
+### Clone the monorepo repository
+
+```
+cd openfn/adaptors
+git clone https://github.com/OpenFn/adaptors.git
+```
+
+### Install the build tools
+
+```
+asdf install # Install tool versions
+pnpm install
+pnpm build
+pnpm run setup
+```
+
+### Switch to the working branch of the varo adaptor
+
+```
+cd openfn/adaptors/adaptors
+git checkout nhgh-varo
+```
+
--- packages/varo/src/Adaptor.js
+++ packages/varo/src/Adaptor.js
@@ -1,5 +1,17 @@
 import { composeNextState } from '@openfn/language-common';
 
+import { parseMetadata } from './Utils';
+import { parseVaroEmsToEms } from './VaroEmsUtils';
+import { parseFridgeTag, parseFridgeTagToEms } from './FridgeTagUtils';
+
+/**
+ * Processes EMS data from the provided state object.
+ *
+ * Iterates over messages in the state, validates their metadata and content,
+ * parses the data, and transforms it into EMS-compatible results.
+ *
+ * @returns {Operation}
+ */
 export function getEmsData() {
   return async state => {
     const results = [];
@@ -7,377 +19,36 @@ export function getEmsData() {
     console.log('--- Incoming message count', state.data.length);
 
     for (const message of state.data) {
-      if (message.fridgeTag) {
-        if (!message.metadata?.content) {
-          console.error('No metadata supplied.');
-          continue;
-        }
+      if (message.fridgeTag?.content) {
+        const metadata = parseMetadata(message);
+        if (!metadata) continue;
 
-        if (!message.fridgeTag?.content) {
-          console.error('No fridgetag supplied.');
-          continue;
-        }
-
-        const metadata = JSON.parse(message.metadata.content);
         const fridgeTagNodes = parseFridgeTag(message.fridgeTag.content);
-
         const result = parseFridgeTagToEms(metadata, fridgeTagNodes);
         results.push(result);
         continue;
       }
 
-      if (message.data) {
-        if (!message.metadata?.content) {
-          console.error('No metadata supplied.');
-          continue;
-        }
+      if (message.data?.content) {
+        const metadata = parseMetadata(message);
+        if (!metadata) continue;
 
-        if (!message.data?.content) {
-          console.error('No data supplied.');
-          continue;
-        }
-
-        const metadata = JSON.parse(message.metadata.content);
         const data = JSON.parse(message.data.content);
         const dataPath = message.data.filename;
-
         const result = parseVaroEmsToEms(metadata, data, dataPath);
         results.push(result);
         continue;
       }
 
-      throw new Error(
+      console.error(
         `Insufficient content found for MessageID: ${message.messageId}`
       );
     }
 
     console.log('--- Converted message count', results.length);
 
-    const nextState = { ...composeNextState(state, results) };
-
-    return nextState;
+    return { ...composeNextState(state, results) };
   };
-}
-
-function parseVaroEmsToEms(metadata, data, dataPath) {
-  const result = {
-    CID: null,
-    LAT: metadata.location.used.latitude,
-    LNG: metadata.location.used.longitude,
-    ADOP: data.ADOP,
-    AMFR: data.AMFR,
-    AMOD: data.AMOD,
-    APQS: data.APQS,
-    ASER: data.ASER,
-    EDOP: null,
-    EMFR: null,
-    EMOD: null,
-    EPQS: null,
-    ESER: null,
-    EMSV: null,
-    LDOP: data.LDOP,
-    LMFR: data.LMFR,
-    LMOD: data.LMOD,
-    LPQS: data.LPQS,
-    LSER: data.LSER,
-    LSV: data.LSV,
-    records: [],
-  };
-
-  const deviceDate = getRelativeDateFromUsbPluggedInfo(dataPath);
-
-  for (const item of data.records) {
-    const durations = [item.RELT]; // ignore RTCW?
-    const absoluteDate = getAdjustedDate(deviceDate, durations);
-    const abst = parseIsoToAbbreviatedIso(absoluteDate);
-
-    const record = {
-      ABST: abst, // absolute time utc
-      ALRM: null, // presence of defined alarm conditions
-      BEMD: null, // emd - estimated number of days of battery life remaining
-      BLOG: item.BLOG, // logger - estimated number of days of battery life remaining
-      CMPR: null, // compressor operation seconds per 15 minutes
-      DORV: item.DORV, // vaccine compartment open seconds per 15 minutes
-      HAMB: null, // relative humidity
-      HOLD: item.HOLD, // days the vaccine department will stay cold if power disconnected
-      LERR: item.LERR, // logger errors
-      EERR: null, // emd errors
-      SVA: null, // good voltage in seconds per 15 minutes
-      TAMB: item.TAMB, // ambient temperature
-      TVC: item.TVC, // vaccine chamber temperature
-    };
-
-    // TODO: limit the number of records for testing.
-    if (result.records.length >= 3) {
-      result.records.shift();
-    }
-
-    result.records.push(record);
-  }
-
-  return result;
-}
-
-function getRelativeDateFromUsbPluggedInfo(path) {
-  const regex = /_CURRENT_DATA_(?<duration>\w+?)_(?<date>\w+?)\.json$/;
-  const match = path.match(regex);
-
-  if (!match) {
-    throw new Error(`Path format is incorrect: ${path}`);
-  }
-
-  const usbPluggedInfo = {
-    date: match.groups.date,
-    duration: match.groups.duration,
-  };
-
-  const isoDate = parseAbbreviatedIsoToIso(usbPluggedInfo.date);
-
-  // The relative date is: from when the USB drive was plugged in, the date, minus the relative duration.
-  return getAdjustedDate(isoDate, [usbPluggedInfo.duration], true);
-}
-
-function getAdjustedDate(incomingDate, durations, subtract = false) {
-  // Parse duration in the format PnDTnHnMnS.
-  function parseDuration(duration) {
-    const regex =
-      /^P((?<days>\d+)D)?(T((?<hours>\d+)H)?((?<minutes>\d+)M)?((?<seconds>\d+)S)?)?$/;
-    const match = duration.match(regex);
-
-    if (!match) {
-      throw new Error(`Invalid duration format: ${duration}`);
-    }
-
-    const multiplier = subtract ? -1 : 1;
-    const days = parseInt(match.groups.days || 0, 10) * multiplier;
-    const hours = parseInt(match.groups.hours || 0, 10) * multiplier;
-    const minutes = parseInt(match.groups.minutes || 0, 10) * multiplier;
-    const seconds = parseInt(match.groups.seconds || 0, 10) * multiplier;
-
-    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-      return null;
-    }
-
-    return { days, hours, minutes, seconds };
-  }
-
-  function applyDuration(date, duration) {
-    date.setDate(date.getDate() + duration.days);
-    date.setHours(date.getHours() + duration.hours);
-    date.setMinutes(date.getMinutes() + duration.minutes);
-    date.setSeconds(date.getSeconds() + duration.seconds);
-  }
-
-  const adjustedDate = new Date(incomingDate);
-
-  for (const duration of durations) {
-    if (!duration) continue;
-
-    const parsedDuration = parseDuration(duration);
-    if (!parsedDuration) continue;
-
-    applyDuration(adjustedDate, parsedDuration);
-  }
-
-  return adjustedDate;
-}
-
-function parseAbbreviatedIsoToIso(abbreviatedIso) {
-  // Incoming format is abbreviated ISO 8601: 20240913T171251Z
-  const regex = /^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$/;
-  const match = abbreviatedIso.match(regex);
-
-  if (!match) {
-    throw new Error(`Invalid abbreviated ISO date format: ${abbreviatedIso}`);
-  }
-
-  // Reformat to ISO 8601: YYYY-MM-DDTHH:MM:SSZ
-  return `${match[1]}-${match[2]}-${match[3]}T${match[4]}:${match[5]}:${match[6]}Z`;
-}
-
-function parseIsoToAbbreviatedIso(iso) {
-  return iso
-    .toISOString()
-    .replace(/[\-\:]/g, '')
-    .replace('.000Z', 'Z');
-}
-
-function parseFridgeTagToEms(metadata, nodes) {
-  const result = {
-    records: [],
-    reports: [],
-  };
-
-  applyMetadataToResult();
-  applyNodesToResult();
-
-  function applyMetadataToResult() {
-    applyPropertyToResult('AMFR', metadata.refrigerator?.manufacturer);
-    applyPropertyToResult('AMOD', metadata.refrigerator?.model);
-
-    applyPropertyToResult('CID', metadata.facility?.country);
-
-    applyPropertyToResult('LAT', metadata.location?.used?.latitude);
-    applyPropertyToResult('LNG', metadata.location?.used?.longitude);
-    applyPropertyToResult('LACC', metadata.location?.used?.accuracy);
-
-    applyPropertyToResult('LMFR', metadata.refrigerator?.deviceManufacturer);
-  }
-
-  function applyNodesToResult() {
-    applyPropertyToResult('LMOD', nodes['Device']);
-    applyPropertyToResult('LSER', nodes['Conf']?.['Serial']);
-    applyPropertyToResult('LSV', nodes['Fw Vers']);
-
-    let productionDate = nodes['Conf']?.['Test TS'];
-    productionDate = productionDate ? new Date(productionDate) : null;
-    applyPropertyToResult('LDOP', productionDate);
-
-    let dayIndex = 1;
-    let dayNode;
-    while ((dayNode = nodes['Hist'][dayIndex++]) !== undefined) {
-      applyDayToRecords(dayNode);
-      applyDayToReports(dayNode);
-    }
-  }
-
-  function applyPropertyToResult(property, text) {
-    if (!text) return;
-    result[property] = text;
-  }
-
-  function applyDayToRecords(dayNode) {
-    applyNodeToRecords(dayNode, 'TS Min T', 'Min T', '0', 'FRZE');
-    applyNodeToRecords(dayNode, 'TS Max T', 'Max T', '1', 'HEAT');
-
-    function applyNodeToRecords(
-      dayNode,
-      timeField,
-      tempField,
-      alarmField,
-      alarmDescription
-    ) {
-      const date = dayNode['Date'];
-      const time = dayNode[timeField];
-      const dateTime = new Date(`${date}T${time}:00Z`);
-      const temp = parseFloat(dayNode[tempField]);
-      const alarm = parseAlarm(dayNode['Alarm'][alarmField], alarmDescription);
-
-      result.records.push({
-        ABST: dateTime,
-        TVC: temp,
-        ALRM: alarm,
-        description: date + ' ' + tempField,
-      });
-
-      function parseAlarm(alarmNode, description) {
-        if (alarmNode['t Acc'] === '0') return null;
-        return description + (alarmNode['C A'] !== '0' ? 'ACK' : '');
-      }
-    }
-  }
-
-  function applyDayToReports(dayNode) {
-    const report = {
-      date: new Date(dayNode['Date']),
-      duration: '1D',
-      alarms: [],
-      aggregates: [],
-    };
-
-    applyAlarmsToReport(dayNode);
-    applyAggregatesToReport(dayNode);
-
-    result.reports.push(report);
-
-    function applyAlarmsToReport(dayNode) {
-      const date = dayNode['Date'];
-
-      applyAlarmToAlarms(dayNode['Alarm']['0'], 'FRZE');
-      applyAlarmToAlarms(dayNode['Alarm']['1'], 'HEAT');
-
-      function applyAlarmToAlarms(alarmNode, description) {
-        const minutes = parseInt(alarmNode['t Acc']);
-        if (!minutes) return;
-
-        const alarm = {
-          condition: description,
-          conditionMinutes: minutes,
-        };
-
-        const time = alarmNode['TS A'];
-        if (time) {
-          alarm.alarmTime = new Date(`${date}T${time}:00Z`);
-        }
-
-        const count = parseInt(alarmNode['C A']);
-        if (count) {
-          alarm.count = count;
-        }
-
-        report.alarms.push(alarm);
-      }
-    }
-
-    function applyAggregatesToReport(dayNode) {
-      applyTvcToAggregates(dayNode);
-
-      function applyTvcToAggregates(dayNode) {
-        report.aggregates.push({
-          id: 'TVC',
-          min: parseFloat(dayNode['Min T']),
-          max: parseFloat(dayNode['Max T']),
-          average: parseFloat(dayNode['Avrg T']),
-        });
-      }
-    }
-  }
-
-  return result;
-}
-
-function parseFridgeTag(text) {
-  const result = {};
-  const properties = [{ indent: -1, property: result }];
-
-  for (const line of text.split('\n')) {
-    const trimmedLine = line.trim();
-    if (!trimmedLine) continue;
-
-    // Determine current indent level.
-    const indent = line.search(/\S/);
-
-    // If our indent is the same or smaller, it indicates a new property; remove the finished property.
-    while (indent <= properties[properties.length - 1].indent) {
-      properties.pop();
-    }
-
-    const property = properties[properties.length - 1].property;
-    const segments = trimmedLine.split(',').map(s => s.trim());
-
-    for (const segment of segments) {
-      const pair = getKeyValuePair(segment);
-      if (!pair) continue;
-
-      if (pair.value) {
-        property[pair.key] = pair.value;
-      } else {
-        // If the value is empty, create a child property.
-        property[pair.key] = {};
-        properties.push({ indent, property: property[pair.key] });
-      }
-    }
-  }
-
-  return result;
-
-  function getKeyValuePair(segment) {
-    const colonIndex = segment.indexOf(':');
-    if (colonIndex === -1) return null;
-    const key = segment.slice(0, colonIndex).trim();
-    const value = segment.slice(colonIndex + 1).trim();
-    return { key, value };
-  }
 }
 
 export {
