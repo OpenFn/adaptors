a5585062d48a0ca5e004228c3b5456988843892f
Emmanuel Evance <mtuchidev@gmail.com>
2025-03-21 13:28:41 +0300

    remove sinon

--- packages/salesforce/test/http.test.js
+++ packages/salesforce/test/http.test.js
@@ -1,37 +1,30 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
-
-import * as http from '../src/http';
+import { http, setMockConnection } from '../src/index';
 
 describe('http', () => {
   describe('request', () => {
-    let spy;
-    const fakeConnection = {
-      request: () => Promise.resolve({ label: 'Account' }),
-    };
-    let state = { connection: fakeConnection };
-
+    let reqArgs;
     beforeEach(() => {
-      spy = sinon.spy(fakeConnection, 'request');
-    });
+      const fakeConnection = {
+        request: args => {
+          reqArgs = args;
+          return Promise.resolve({ label: 'Account' });
+        },
+      };
 
-    afterEach(() => {
-      spy.restore(); // This restores all Sinon stubs and spies
+      setMockConnection(fakeConnection);
     });
 
     it('should use relative path from root', done => {
       http
-        .request('/services/data/v46.0/sobjects/Account/describe')(state)
+        .request('/services/data/v46.0/sobjects/Account/describe')({})
         .then(({ data }) => {
-          expect(spy.args[0]).to.eql([
-            {
-              body: undefined,
-              headers: undefined,
-              method: 'GET',
-              url: '/services/data/v46.0/sobjects/Account/describe',
-            },
-          ]);
-          expect(spy.called).to.eql(true);
+          expect(reqArgs).to.eql({
+            body: undefined,
+            headers: { 'content-type': 'application/json' },
+            method: 'GET',
+            url: '/services/data/v46.0/sobjects/Account/describe',
+          });
           expect(data.label).to.eq('Account');
         })
         .then(done)
@@ -40,17 +33,14 @@ describe('http', () => {
 
     it('should use relative path from version root', done => {
       http
-        .request('/sobjects/Account/describe')(state)
+        .request('/sobjects/Account/describe')({})
         .then(({ data }) => {
-          expect(spy.args[0]).to.eql([
-            {
-              body: undefined,
-              headers: undefined,
-              method: 'GET',
-              url: '/sobjects/Account/describe',
-            },
-          ]);
-          expect(spy.called).to.eql(true);
+          expect(reqArgs).to.eql({
+            body: undefined,
+            headers: { 'content-type': 'application/json' },
+            method: 'GET',
+            url: '/sobjects/Account/describe',
+          });
           expect(data.label).to.eq('Account');
         })
         .then(done)
@@ -59,14 +49,17 @@ describe('http', () => {
   });
   describe('get', () => {
     it('should append correct query parameters to the request URL', done => {
+      let reqArgs;
       const fakeConnection = {
-        request: () => {
-          return Promise.resolve({ latestDateCovered: '2020-01-01T00:00:00Z' });
+        request: args => {
+          reqArgs = args;
+          return Promise.resolve({
+            latestDateCovered: '2020-01-01T00:00:00Z',
+          });
         },
       };
-      let state = { connection: fakeConnection };
-
-      let spy = sinon.spy(fakeConnection, 'request');
+      setMockConnection(fakeConnection);
+      const state = {};
 
       const startDate = '2020-01-01T00:00:00Z';
       const endDate = '2020-01-02T00:00:00Z';
@@ -75,13 +68,11 @@ describe('http', () => {
           query: { start: startDate, end: endDate },
         })(state)
         .then(state => {
-          const reqArgs = spy.args.flat()[0];
           expect(reqArgs.url).to.include(
             `start=${encodeURIComponent(startDate)}`
           );
           expect(reqArgs.url).to.include(`end=${encodeURIComponent(endDate)}`);
           expect(reqArgs.method).to.eql('GET');
-          expect(spy.called).to.eql(true);
           expect(state.data).to.eql({
             latestDateCovered: '2020-01-01T00:00:00Z',
           });
@@ -90,27 +81,26 @@ describe('http', () => {
         .catch(done);
     });
     it('fetches an account record', done => {
+      let reqArgs;
       const fakeConnection = {
-        request: function () {
+        request: args => {
+          reqArgs = args;
           return Promise.resolve({ Id: 10 });
         },
       };
-      let state = { connection: fakeConnection, references: [] };
-
-      let spy = sinon.spy(fakeConnection, 'request');
+      setMockConnection(fakeConnection);
+      const state = {};
 
       http
         .get('/services/data/v58.0/sobjects/Account/10')(state)
         .then(state => {
-          const reqArgs = spy.args.flat()[0];
-
           expect(reqArgs).to.eql({
             body: undefined,
-            headers: undefined,
+            headers: { 'content-type': 'application/json' },
             method: 'GET',
             url: '/services/data/v58.0/sobjects/Account/10',
           });
-          expect(spy.called).to.eql(true);
+
           expect(state.data).to.eql({ Id: 10 });
         })
         .then(done)
@@ -119,8 +109,10 @@ describe('http', () => {
   });
   describe('post', () => {
     it('should post a request to Salesforce', done => {
+      let reqArgs;
       const fakeConnection = {
-        request: () => {
+        request: args => {
+          reqArgs = args;
           return Promise.resolve({
             id: '0015g00000LJ2wGAAT',
             success: true,
@@ -128,16 +120,14 @@ describe('http', () => {
           });
         },
       };
-      let state = { connection: fakeConnection };
-
-      let spy = sinon.spy(fakeConnection, 'request');
+      setMockConnection(fakeConnection);
+      const state = {};
 
       http
         .post('/sobjects/Account', {
           Name: 'test',
         })(state)
         .then(state => {
-          const reqArgs = spy.args.flat()[0];
           expect(reqArgs).to.eql({
             body: JSON.stringify({ Name: 'test' }),
             headers: {
@@ -146,7 +136,7 @@ describe('http', () => {
             method: 'POST',
             url: '/sobjects/Account',
           });
-          expect(spy.called).to.eql(true);
+
           expect(state.data.success).to.eql(true);
         })
         .then(done)
