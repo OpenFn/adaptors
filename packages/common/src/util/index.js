e80640b9ac3d90e588b2d069a2a4b5e6169a732b
Hunter Achieng <achienghunter@gmail.com>
2025-04-30 11:28:21 +0300

    feat: move field to util
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/common/src/util/index.js
+++ packages/common/src/util/index.js
@@ -5,3 +5,4 @@ export { default as parseDate } from './parse-date';
 export { default as throwError } from './throw-error';
 export { encode, decode } from './base64';
 export { uuid } from './uuid';
+export {field} from './field'
