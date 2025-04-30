54e82bbd2d7c21907598e77d90d3e94e85bea715
Hunter Achieng <achienghunter@gmail.com>
2025-04-30 11:28:21 +0300

    feat: move field to util
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/common/src/Adaptor.js
+++ packages/common/src/Adaptor.js
@@ -356,19 +356,6 @@ export function join(targetPath, sourcePath, targetKey) {
   };
 }
 
-/**
- * Returns a key, value pair in an array.
- * @public
- * @function
- * @example
- * field('destination_field_name__c', 'value')
- * @param {string} key - Name of the field
- * @param {Value} value - The value itself or a sourceable operation.
- * @returns {Field}
- */
-export function field(key, value) {
-  return [key, value];
-}
 
 /**
  * Zips key value pairs into an object.
