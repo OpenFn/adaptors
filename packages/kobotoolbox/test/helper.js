a94fcdeaea8f9ed47db2c5af72da2d9aa22d43f0
Emmanuel Evance <mtuchidev@gmail.com>
2025-05-14 23:38:10 +0300

    update responseWithPagination helper

--- packages/kobotoolbox/test/helper.js
+++ packages/kobotoolbox/test/helper.js
@@ -1,6 +1,11 @@
-//TODO: We should track iterations for testing purpose
+import { DEFAULT_REQUEST_LIMIT } from '../src/util';
+
 export function responseWithPagination(items, query, settings) {
-  const { url, defaultLimit = 1e3, maxLimit = 3e4 } = settings;
+  const {
+    url,
+    defaultLimit = DEFAULT_REQUEST_LIMIT,
+    maxLimit = DEFAULT_REQUEST_LIMIT,
+  } = settings;
   const { start = 0, limit } = query;
 
   const response = {
@@ -13,60 +18,61 @@ export function responseWithPagination(items, query, settings) {
 
   let startIndex = start;
   const hasStartIndex = startIndex > 0;
-  console.log({ startIndex });
   let shouldFetchMoreContent = false;
-  let effectiveLimit = limit ?? maxLimit;
-  // if (limit) {
-  //   effectiveLimit = Math.min(limit, maxLimit);
-  // }
-  // if (!limit && defaultLimit !== 1e3) {
-  //   effectiveLimit = defaultLimit;
-  // }
-  // if (!limit && defaultLimit === 1e3) {
-  //   effectiveLimit = Math.min(maxLimit, items.length);
-  // }
-  let endIndex = Math.min(startIndex + effectiveLimit, items.length);
+  let effectiveLimit = limit ?? defaultLimit;
+  let isUsingDefaultLimit = !limit && defaultLimit === DEFAULT_REQUEST_LIMIT;
+  console.log({ isUsingDefaultLimit, limit, defaultLimit });
 
-  console.log({ effectiveLimit });
+  let maxResults = maxLimit;
+  const hasNoLimitWithStartIndex = !limit && hasStartIndex;
+  if (hasNoLimitWithStartIndex) {
+    maxResults = items.length - startIndex;
+  }
+
   do {
     response.results.push(
       ...items.slice(startIndex, startIndex + effectiveLimit)
     );
-    const reachedLimit = effectiveLimit >= response.results.length;
-    shouldFetchMoreContent =
-      !reachedLimit && response.results.length < maxLimit;
+    const fetchedAll =
+      response.results.length ===
+      (limit < items.length ? effectiveLimit : items.length);
+    const reachedMaxLimit = response.results.length >= maxResults;
+
+    shouldFetchMoreContent = !fetchedAll && !reachedMaxLimit;
 
     response.requestCount++;
-    startIndex += defaultLimit;
+    startIndex += effectiveLimit;
 
-    if (effectiveLimit >= items.length) {
-      response.next = null;
-      response.previous =
-        effectiveLimit === maxLimit && hasStartIndex
-          ? `${url}?format=json&limit=${effectiveLimit}`
-          : null;
-    } else {
-      response.next = `${url}?format=json&start=${startIndex}&limit=${effectiveLimit}`;
+    response.next =
+      limit > items.length || // If there are less items than the limit, return all and no next link
+      (!limit && !hasStartIndex && fetchedAll) || // If there are no items, start = 0 and no limit, return all and no next link
+      (fetchedAll && hasNoLimitWithStartIndex) // If there are more items, no limit and start > 0, return all and no next link
+        ? null
+        : `${url}?format=json&start=${startIndex}&limit=${effectiveLimit}`;
 
-      response.previous =
-        startIndex <= defaultLimit
-          ? null
-          : `${url}?format=json&start=${Math.max(
-              0,
-              startIndex - defaultLimit
-            )}&limit=${effectiveLimit}`;
-    }
+    response.previous =
+      !hasStartIndex && startIndex <= effectiveLimit
+        ? null
+        : hasNoLimitWithStartIndex
+        ? `${url}?format=json&limit=${maxLimit}` // If no limit and start >0
+        : `${url}?format=json&start=${Math.max(
+            0,
+            startIndex - effectiveLimit
+          )}&limit=${effectiveLimit}`;
 
     console.log({
+      fetchedAll,
+      hasNoLimitWithStartIndex,
+      hasStartIndex,
       effectiveLimit,
-      endIndex,
+      maxResults,
       startIndex,
       defaultLimit,
       shouldFetchMoreContent,
       currentResults: response.results.length,
-      reachedLimit,
+      Req: response.requestCount,
+      response,
     });
   } while (shouldFetchMoreContent);
-
   return response;
 }
