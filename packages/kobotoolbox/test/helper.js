f13122b351818e5e22c7eb434f10d31bf3fcf67b
Emmanuel Evance <mtuchidev@gmail.com>
2025-05-16 01:05:13 +0300

    add default max limit

--- packages/kobotoolbox/test/helper.js
+++ packages/kobotoolbox/test/helper.js
@@ -1,10 +1,10 @@
-import { DEFAULT_REQUEST_LIMIT } from '../src/util';
+import { DEFAULT_REQUEST_LIMIT, DEFAULT_MAX_LIMIT } from '../src/util';
 
 export function responseWithPagination(items, query, settings) {
   const {
     url,
     defaultLimit = DEFAULT_REQUEST_LIMIT,
-    maxLimit = DEFAULT_REQUEST_LIMIT,
+    maxLimit = DEFAULT_MAX_LIMIT,
   } = settings;
   const { start = 0, limit } = query;
 
@@ -16,12 +16,12 @@ export function responseWithPagination(items, query, settings) {
     requestCount: 0,
   };
 
-  let startIndex = start;
+  let startIndex = parseInt(start);
   const hasStartIndex = startIndex > 0;
   let shouldFetchMoreContent = false;
   let effectiveLimit = limit ?? defaultLimit;
   let isUsingDefaultLimit = !limit && defaultLimit === DEFAULT_REQUEST_LIMIT;
-  console.log({ isUsingDefaultLimit, limit, defaultLimit });
+  // console.log({ isUsingDefaultLimit, limit, defaultLimit });
 
   let maxResults = maxLimit;
   const hasNoLimitWithStartIndex = !limit && hasStartIndex;
@@ -44,6 +44,7 @@ export function responseWithPagination(items, query, settings) {
     startIndex += effectiveLimit;
 
     response.next =
+      startIndex === items.length ||
       limit > items.length || // If there are less items than the limit, return all and no next link
       (!limit && !hasStartIndex && fetchedAll) || // If there are no items, start = 0 and no limit, return all and no next link
       (fetchedAll && hasNoLimitWithStartIndex) // If there are more items, no limit and start > 0, return all and no next link
@@ -53,7 +54,7 @@ export function responseWithPagination(items, query, settings) {
     response.previous =
       !hasStartIndex && startIndex <= effectiveLimit
         ? null
-        : hasNoLimitWithStartIndex
+        : hasNoLimitWithStartIndex && isUsingDefaultLimit
         ? `${url}?format=json&limit=${maxLimit}` // If no limit and start >0
         : `${url}?format=json&start=${Math.max(
             0,
