49113b35c0b30ae0f918704fd81f64446c1f865c
pius <piuskariuki19@gmail.com>
2025-06-30 16:06:37 +0300

    Changed example phone number

--- packages/whatsapp/src/Adaptor.js
+++ packages/whatsapp/src/Adaptor.js
@@ -28,7 +28,7 @@ import * as util from './Utils';
  * Make a general HTTP request
  * @example
  * <caption>Send a whatsapp message</caption>
- * request('POST', 'messages',{ to: '254757240608', body: 'Hello, world!', type: 'template', template: { name: 'hello_world', language: { code: 'en_US' } }, messaging_product: 'whatsapp' });
+ * request('POST', 'messages',{ to: '254712345678', body: 'Hello, world!', type: 'template', template: { name: 'hello_world', language: { code: 'en_US' } }, messaging_product: 'whatsapp' });
  * @function
  * @public
  * @param {string} method - HTTP method to use
