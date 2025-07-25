e6fd2a3bb5a325e3418eb34a91af815d9dab8c4a
hunterachieng <79210550+hunterachieng@users.noreply.github.com>
2025-07-25 10:11:00 +0300

    dhis2: fix dhis2 metadata (#1294)
    * fix: update metadata helper and config
    
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>
    
    * feat: update metadata
    
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>
    
    * fix: change host url to `dev`
    
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>
    
    * fix config url
    
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>
    
    * versions
    
    ---------
    
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>
    Co-authored-by: Joe Clark <joe@openfn.org>

--- packages/dhis2/src/meta/config.js
+++ packages/dhis2/src/meta/config.js
@@ -1,7 +1,7 @@
 // Config for dhis2 sandbox
 export default {
   configuration: {
-    hostUrl: 'https://play.dhis2.org/40.3.0',
+    hostUrl: 'https://play.im.dhis2.org/dev',
     username: process.env.OPENFN_DHIS2_USER,
     password: process.env.OPENFN_DHIS2_PW,
   },
