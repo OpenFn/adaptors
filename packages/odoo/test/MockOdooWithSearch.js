8864b7bdd7b37b78aac04b2e4e392b00f1a5ffb7
Hunter Achieng <achienghunter@gmail.com>
2025-08-07 23:00:06 +0300

    fix: mock Odoo class function
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/odoo/test/MockOdooWithSearch.js
+++ packages/odoo/test/MockOdooWithSearch.js
@@ -1,3 +1,5 @@
+import testData from './odooFixtures.json' assert { type: 'json' };
+
 class MockOdooWithSearch {
   // this can be whatever we want
   constructor(data = []) {
@@ -6,11 +8,21 @@ class MockOdooWithSearch {
 
   // this needs to match the odoo client signature
   searchRead(model, domain, fields, args) {
-    const { offset, pageSize, query } = args;
+    const { offset = 0, limit } = args;
 
     const result = this.data
-      .filter(/* do some kind of search */)
-      .slice(offset, offset + pageSize);
+      .filter(item => {
+        return (
+          item['im_status'].includes(model) &&
+          item['is_company'] === domain['is_company']
+        );
+      })
+      .map(item => {
+        return Object.fromEntries(
+          Object.entries(item).filter(([key]) => fields.includes(key))
+        );
+      })
+      .slice(offset, offset + limit);
 
     return result;
   }
@@ -19,8 +31,13 @@ class MockOdooWithSearch {
 // first we unit test this
 // these are NOT pagination tests
 // each just gets a single "page" of data
-// const mock = new MockOdooWithSearch(data);
-// const result = mock.searchRead({ limit: 10 });
+
+const mock = new MockOdooWithSearch(testData);
+const result = mock.searchRead('partner', { is_company: true }, ['name'], {
+  limit: 10,
+});
+console.log({ result });
+
 // expect(result.length).to.equal(10);
 
 // Then we write unit tests against the new operation with pagination
