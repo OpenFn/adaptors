bbe158c0fef19e9b5b65571b5727cd38cb3aa1ff
Hunter Achieng <achienghunter@gmail.com>
2025-08-11 18:15:22 +0300

    fix: add mock odoo tests
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/odoo/test/MockOdooWithSearch.js
+++ packages/odoo/test/MockOdooWithSearch.js
@@ -1,12 +1,10 @@
 import testData from './odooFixtures.json' assert { type: 'json' };
 
-class MockOdooWithSearch {
-  // this can be whatever we want
+export class MockOdooWithSearch {
   constructor(data = []) {
     this.data = data;
   }
 
-  // this needs to match the odoo client signature
   searchRead(model, domain, fields, args) {
     const { offset = 0, limit } = args;
 
@@ -28,22 +26,8 @@ class MockOdooWithSearch {
   }
 }
 
-// first we unit test this
-// these are NOT pagination tests
-// each just gets a single "page" of data
-
 const mock = new MockOdooWithSearch(testData);
 const result = mock.searchRead('partner', { is_company: true }, ['name'], {
   limit: 10,
 });
 console.log({ result });
-
-// expect(result.length).to.equal(10);
-
-// Then we write unit tests against the new operation with pagination
-// const data = [item1, item2, item100];
-// const mock = new MockOdooWithSearch(data);
-
-// setMockClient(mock);
-
-// searchReadRecord({ offset: 100, pageSize: 10 });
