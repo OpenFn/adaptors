a38535ddc5026be95b08d0a69dd8dcbe9e04fba2
mtuchi <mtuchidev@gmail.com>
2025-08-28 16:22:34 +0300

    update request function

--- packages/memento/src/http.js
+++ packages/memento/src/http.js
@@ -1,6 +1,6 @@
 import { expandReferences } from '@openfn/language-common/util';
 
-import * as util from './util';
+import { request as sendRequest } from './util';
 
 /**
  * State object
@@ -15,13 +15,9 @@ import * as util from './util';
  * @typedef {Object} RequestOptions
  * @public
  * @property {object|string} body - body data to append to the request. JSON will be converted to a string (but a content-type header will not be attached to the request).
- * @property {object} errors - Map of errorCodes -> error messages, ie, `{ 404: 'Resource not found;' }`. Pass `false` to suppress errors for this code.
- * @property {object} form - Pass a JSON object to be serialised into a multipart HTML form (as FormData) in the body.
  * @property {object} query - An object of query parameters to be encoded into the URL.
  * @property {object} headers - An object of headers to append to the request.
  * @property {string} parseAs - Parse the response body as json, text or stream. By default will use the response headers.
- * @property {number} timeout - Request timeout in ms. Default: 300 seconds.
- * @property {object} tls - TLS/SSL authentication options. See https://nodejs.org/api/tls.html#tlscreatesecurecontextoptions
  */
 
 /**
@@ -70,19 +66,12 @@ export function post(path, body, options) {
  */
 export function request(method, path, body, options = {}) {
   return async state => {
-    const [resolvedMethod, resolvedPath, resolvedBody, resolvedoptions] =
+    const [resolvedMethod, resolvedPath, resolvedBody, resolvedOptions] =
       expandReferences(state, method, path, body, options);
 
-    const response = await util.request(
-      state.configuration,
-      resolvedMethod,
-      resolvedPath,
-      {
-        body: resolvedBody,
-        ...resolvedoptions,
-      }
-    );
-
-    return util.prepareNextState(state, response);
+    return sendRequest(state, resolvedMethod, resolvedPath, {
+      ...resolvedOptions,
+      body: resolvedBody,
+    });
   };
 }
