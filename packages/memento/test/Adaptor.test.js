e2b22bb17dc08828a48738f97f17940e500ac04b
mtuchi <mtuchidev@gmail.com>
2025-08-29 18:16:51 +0300

    add getEntries test

--- packages/memento/test/Adaptor.test.js
+++ packages/memento/test/Adaptor.test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
-import { listLibraries } from '../src/Adaptor.js';
+import { mockEntriesPagination } from './helpers';
+import { listLibraries, getEntries } from '../src/Adaptor.js';
 import { enableMockClient } from '@openfn/language-common/util';
 
 const testServer = enableMockClient('https://api.mementodatabase.com');
@@ -10,20 +11,40 @@ const state = {
 };
 
 describe('Adaptor', () => {
-  it('lists libraries', async () => {
-    testServer
-      .intercept({
-        path: '/v1/libraries',
-        query: { token: 'user-api-token' },
-      })
-      .reply(200, {
+  describe('listLibraries', () => {
+    it('lists libraries', async () => {
+      testServer
+        .intercept({
+          path: '/v1/libraries',
+          query: { token: 'user-api-token' },
+        })
+        .reply(200, {
+          libraries: [],
+        });
+
+      const { data } = await listLibraries()(state);
+
+      expect(data).to.eql({
         libraries: [],
       });
-
-    const { data } = await listLibraries()(state);
-
-    expect(data).to.eql({
-      libraries: [],
     });
+  });
+  describe('getEntries', () => {
+    it('should fetch all entries', async () => {
+      const pageSize = 1;
+      mockEntriesPagination(testServer, '/v1/libraries/HyZV7AYk0/entries', {
+        pageSize,
+        totalPage: 11,
+        fields: 'all',
+      });
+
+      const { data } = await getEntries('HyZV7AYk0', {
+        pageSize,
+        throttleTime: 1000,
+      })(state);
+
+      expect(data.entries.length).to.eql(11);
+      expect(data.nextPageToken).to.eql(undefined);
+    }).timeout(6e4 + 1000);
   });
 });
