ff4db7266d37c0a2eeb7f5f3e2eae42cd4ab15cf
Hunter Achieng <achienghunter@gmail.com>
2025-10-02 13:58:01 +0300

    fix: docs
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

--- packages/et-mfr/src/Adaptor.js
+++ packages/et-mfr/src/Adaptor.js
@@ -5,7 +5,7 @@ import * as util from './Utils.js';
  * State object
  * @typedef {Object} HttpState
  * @property data - the parsed response body
- * @property response - the response from the HTTP server, including headers, statusCode, body, etc
+ * @property response - the response from the HTTP server, including headers, statusCode, etc
  * @property references - an array of all previous data objects used in the Job
  **/
 
@@ -13,9 +13,7 @@ import * as util from './Utils.js';
  * Options provided to the HTTP request
  * @typedef {Object} RequestOptions
  * @public
- * @property {object|string} body - body data to append to the request. JSON will be converted to a string (but a content-type header will not be attached to the request).
- * @property {object} errors - Map of errorCodes -> error messages, ie, `{ 404: 'Resource not found;' }`. Pass `false` to suppress errors for this code.
- * @property {object} form - Pass a JSON object to be serialised into a multipart HTML form (as FormData) in the body.
+ * @property {object|string} body - body data to append to the request.
  * @property {object} query - An object of query parameters to be encoded into the URL.
  * @property {object} headers - An object of headers to append to the request.
  * @property {string} parseAs - Parse the response body as json, text or stream. By default will use the response headers.
@@ -31,7 +29,7 @@ import * as util from './Utils.js';
  *     id: '1071644',
  *   },
  * });
- * @example <caption>Get all facility details with serach options</caption>
+ * @example <caption>Get all facility details with search options</caption>
  * get('Facility/All', {
  *   query: {
  *     name: '010 Kombolcha Health Post',
