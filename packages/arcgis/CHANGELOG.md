8531748d97a14d513062b7695c406332953becdc
Pius Kariuki <piuskariuki19@gmail.com>
2025-10-06 13:12:56 +0300

    ARCGIS adaptor

---
+++ packages/arcgis/CHANGELOG.md
@@ -0,0 +1,5 @@
+# @openfn/language-arcgis
+
+## 1.0.0
+
+Initial release.
---
+++ packages/arcgis/src/Adaptor.js
@@ -0,0 +1,29 @@
+/**
+ * State object
+ * @typedef {Object} HttpState
+ * @property data - the parsed response body
+ * @property response - the response from the HTTP server, including headers, statusCode, body, etc
+ * @property references - an array of all previous data objects used in the Job
+ **/
+
+
+export {
+  as,
+  combine,
+  cursor,
+  dataPath,
+  dataValue,
+  dateFns,
+  each,
+  field,
+  fields,
+  fn,
+  fnIf,
+  group,
+  lastReferenceValue,
+  map,
+  merge,
+  scrubEmojis,
+  sourceValue,
+  util,
+} from '@openfn/language-common';
