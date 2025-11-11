4a9fc6f232566a9b9b11406a267f07df299b244f
Emmanuel Evance <mtuchidev@gmail.com>
2025-11-11 22:46:20 +0300

    improve tests

--- packages/actualbudget/test/util.test.js
+++ packages/actualbudget/test/util.test.js
@@ -1,19 +1,26 @@
 import { expect } from 'chai';
 import { errorHandler } from '../src/util.js';
 
-describe.only('errorHandler', () => {
-  it('should throw an error if the error type is PostError and the message contains "Not Allowed"', async () => {
-    const err = {
-      type: 'PostError',
-      message: 'Not Allowed',
-      meta: {},
-    };
+function createRejectedPromise() {
+  return new Promise((resolve, reject) => {
+    const err = new Error('This is a rejection');
+    err.type = 'rejection';
+    reject(err);
+  });
+}
+describe('errorHandler', () => {
+  it('should show fix if error is a PostError', async () => {
+    const err = new Error('Not Allowed');
+    err.type = 'PostError';
+
     try {
       errorHandler(err);
     } catch (err) {
-      expect(err.message).to.equal(
+      expect(err.message).to.eq('Not Allowed');
+      expect(err.fix).to.eq(
         'Error accessing Actual Server, check Actual Server url'
       );
+      expect(err.type).to.eq('PostError');
     }
   });
 });
