4348d6fb1b7153beac2379e4e1d2e014cca3dde2
Emmanuel Evance <mtuchidev@gmail.com>
2025-12-01 15:54:19 +0300

    unit test sql and insert function

--- packages/mysql/test/index.test.js
+++ packages/mysql/test/index.test.js
@@ -1,30 +1,90 @@
 import { expect } from 'chai';
-import { execute, sql, setMockConnection } from '../src/Adaptor.js';
+import { execute, sql, insert, setMockConnection } from '../src/Adaptor.js';
 
+const configuration = {
+  host: 'localhost',
+  port: 3306,
+  database: 'test',
+  user: 'root',
+  password: 'password',
+};
 describe('sql', () => {
   it('should log and return the query when execute is false', async () => {
     setMockConnection({
-      query: () => {},
-      release: () => {},
+      execute: (sqlStr, values) => {
+        expect(sqlStr).to.eql('SELECT Name, FROM users;');
+        expect(values).to.eql(undefined);
+        return [{ Name: 'New John' }];
+      },
+      end: () => {},
     });
     const fakeState = {
-      configuration: {
-        host: 'localhost',
-        port: 3306,
-        database: 'test',
-        user: 'root',
-        password: 'password',
-      },
+      configuration,
       data: { tableName: 'users' },
     };
 
     const result = await execute(
       sql(state => `SELECT Name, FROM ${state.data.tableName};`, {
         writeSql: true,
-        execute: false,
       })
     )(fakeState);
 
     expect(result.queries).to.include('SELECT Name, FROM users;');
+    expect(result.data.result).to.eql({ Name: 'New John' });
+  });
+  it('shoudld escape sqli inputs', async () => {
+    setMockConnection({
+      end: () => {},
+      execute: async sql => {
+        expect(sql).to.eql(
+          "SELECT order_id,location FROM '\\' OR \\'1\\'=\\'1\\'; --';"
+        );
+
+        throw new Error(
+          "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ''' OR '1'='1'; --'' at line 1"
+        );
+      },
+    });
+
+    const state = {
+      configuration,
+      data: { tableName: "' OR '1'='1'; --" },
+    };
+
+    try {
+      await execute(
+        sql(`SELECT order_id,location FROM ?;`, {
+          values: state => [state.data.tableName],
+          writeSql: true,
+        })
+      )(state);
+    } catch (err) {
+      expect(err.message).to.contain('You have an error in your SQL syntax;');
+    }
+  });
+});
+
+describe('insert', () => {
+  it('should insert a record', async () => {
+    setMockConnection({
+      end: () => {},
+      execute: async (sql, values) => {
+        expect(sql).to.eql(
+          'INSERT INTO `users` (`address`, `name`) VALUES (?, ?)'
+        );
+        expect(values).to.eql(['Ave Dela Casta', 'Alice']);
+        return [{}, []];
+      },
+    });
+    const fakeState = {
+      configuration,
+      data: { address: 'Ave Dela Casta', name: 'Alice' },
+    };
+
+    const { data } = await execute(insert('users', state => state.data))(
+      fakeState
+    );
+    expect(data.result).to.eql({});
+    expect(data.fields).to.eql([]);
   });
 });
--- packages/mysql/test/integration.js
+++ packages/mysql/test/integration.js
@@ -1,21 +1,88 @@
 import { expect } from 'chai';
-import { execute, sql } from '../src/Adaptor.js';
-import configuration from './tmp/creds.json' with { type: 'json' };
+import { execute, sql, insert } from '../src/Adaptor.js';
+import configuration from '../tmp/creds.json' with { type: 'json' };
 
 describe('sql', () => {
   it('should fetch data from the database', async () => {
     const state = {
       configuration,
-      data: { tableName: 'users' },
+      data: { tableName: 'test_order' },
     };
 
-    const result = await execute(
-      sql(state => `SELECT Name, FROM ${state.data.tableName};`)
+    const { data } = await execute(
+      sql(
+        state =>
+          `SELECT order_id,location FROM ${state.data.tableName} limit 1;`
+      )
     )(state);
 
-    expect(result.response.body).to.deep.equal([
-      { Name: 'Alice' },
-      { Name: 'Bob' },
-    ]);
+    expect(data.result).to.deep.equal([{ order_id: 0, location: 'New York' }]);
+    expect(data.fields.length).to.equal(2);
+  });
+  it('should support prepared statements', async () => {
+    const state = {
+      configuration,
+      data: { tableName: 'test_order', order_id: 0 },
+    };
+
+    const { data } = await execute(
+      sql(`SELECT order_id,location FROM ?? where order_id = ?;`, {
+        values: state => [state.data.tableName, state.data.order_id],
+      })
+    )(state);
+
+    expect(data.result).to.deep.equal([{ order_id: 0, location: 'New York' }]);
+    expect(data.fields.length).to.equal(2);
+  });
+  it('should escape sqli inputs', async () => {
+    const state = {
+      configuration,
+      data: { tableName: "' OR '1'='1'; --" },
+    };
+
+    try {
+      await execute(
+        sql(`SELECT order_id,location FROM ?;`, {
+          values: state => [state.data.tableName],
+          writeSql: true,
+        })
+      )(state);
+    } catch (err) {
+      console.log('Caught error as expected:', err.message);
+      expect(err.message).to.contain('You have an error in your SQL syntax;');
+    }
+  });
+});
+
+describe('insert', () => {
+  it('should insert a record', async () => {
+    const state = {
+      configuration,
+      data: {
+        order_id: Math.floor(Math.random() * 1000)
+          .toString()
+          .padStart(3, '0'),
+        specimen_source: 'Blood',
+        laterality: 'left',
+        clinical_history: 'No known allergies',
+        frequency: 'Once',
+        number_of_repeats: 1,
+        location: 'New York',
+      },
+    };
+
+    const { data } = await execute(insert('test_order', state => state.data))(
+      state
+    );
+    expect(data.result).to.eql({
+      fieldCount: 0,
+      affectedRows: 1,
+      insertId: 0,
+      info: '',
+      serverStatus: 2,
+      warningStatus: 0,
+      changedRows: 0,
+    });
+    expect(data.fields).to.eql(undefined);
   });
 });
