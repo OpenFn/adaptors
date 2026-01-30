3aa0ab801f3a1cdaec00e0cf22dbb0a5eff13071
Joe Clark <joe@openfn.org>
2026-01-30 11:34:23 +0000

    remove test file

--- test-url.js
+++
@@ -1,8 +0,0 @@
-
-import path from 'node:path';
-import { pathToFileURL } from 'node:url';
-
-const p = path.join('C:\\Users', 'test.js');
-console.log('Path:', p);
-console.log('URL toString:', pathToFileURL(p).toString());
-console.log('URL href:', pathToFileURL(p).href);
