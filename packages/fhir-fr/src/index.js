e82b006d1b37333312a51613072bd95357065f7a
Joe Clark <joe@openfn.org>
2026-02-03 17:44:43 +0000

    fhir-fr: remove old js code

--- packages/fhir-fr/src/index.js
+++
@@ -1,9 +0,0 @@
-import * as Adaptor from './Adaptor';
-export default Adaptor;
-
-export * from './Adaptor';
-
-export * as util from './utils';
-
-export * as builders from './builders';
-export * as b from './builders';
