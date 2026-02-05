a78d53e1fb048da1c36ed53e0b130828ca4f8db4
Joe Clark <joe@openfn.org>
2026-02-05 14:47:06 +0000

    fix exports

--- tools/generate-fhir/template/src/index.ts
+++ tools/generate-fhir/template/src/index.ts
@@ -1,11 +1,5 @@
 import * as Adaptor from './Adaptor';
 export default Adaptor;
 export * from './Adaptor';
-
-import * as datatypes from './datatypes';
-import * as builders from './builders';
-const b = {
-  ...datatypes,
-  ...builders,
-};
-export { b, b as builders };
+export * as builders from './builders';
+export * as b from './builders';
