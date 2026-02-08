677d337b3657dcdf8be67faf70ff2d22e76daca2
triskyi <tresorbi29@gmail.com>
2026-02-08 04:06:06 -0800

    modified:   packages/browserless/src/Adaptor.js

--- packages/browserless/src/Adaptor.js
+++ packages/browserless/src/Adaptor.js
@@ -1,6 +1,8 @@
 import { expandReferences } from "@openfn/language-common/util";
 import * as util from './Utils.js';
 import { request as httpRequest } from './http.js';
+import {request as lowelLevelRequest} from './Utils.js';
+import { OperationCanceledException } from "typescript";
 
 /**
  * Create a PDF from HTML or URL.
@@ -32,4 +34,45 @@ export function createPDF(input, options){
   };  
   };
 }
+
+
+/**
+ * Generic Browserless-authnticated HTTP request operation.
+ * Use for arbitrary Browserless  endpoints (for example `/convert`).
+ * @public
+ * @function
+ * @param {string} method - HTTP method (e.g. 'GET', 'POST').
+ * @param {string} path - Path or URL to request (relative paths are joined to baseUrl).
+ * @param {object} options - Optional request options.
+ * @returns {Operation} Returns state with `data` and `response`.
+ * 
+ */
+
+export function request(method, path, options){
+  return lowelLevelRequest(undefined, method, path, options);
+}
+
+export{
+  as,
+  combine,
+  cursor,
+  dataPath,
+  dataValue,
+  dateFns,
+  each,
+  field,
+  fields,
+  fn,
+  fnIf,
+  group,
+  lastReferenceValue,
+  map,
+  merge,
+  scrubEmojis,
+  source,
+  sourceValue,
+  util,
+} from '@openfn/language-common'
+
+
 
