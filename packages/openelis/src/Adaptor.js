3fb0392dceb16275ee4c5233533aa0aa720b1812
Pius Kariuki <39379012+PiusKariuki@users.noreply.github.com>
2026-02-16 19:28:55 +0300

    New OpenELIS Adaptor (#1533)
    * First commit
    
    * fix: handle errors in request function by removing catch block
    
    * First commit
    
    * fix: handle errors in request function by removing catch block
    
    * added lock file
    
    * Move functions to http namespace
    
    * test: add unit tests for HTTP request and response handling
    
    * Hardcode repeating parts of the url
    
    * Update examples with http namespace
    
    * chore: update CHANGELOG and remove fhirStoreUri from configuration schema
    
    * chore: fix formatting in CHANGELOG for http helper functions

---
+++ packages/openelis/src/Adaptor.js
@@ -0,0 +1,22 @@
+
+
+export {
+  as,
+  combine,
+  cursor,
+  dataPath,
+  dataValue,
+  dateFns,
+  each,
+  field,
+  fields,
+  fn,
+  fnIf,
+  group,
+  lastReferenceValue,
+  map,
+  merge,
+  scrubEmojis,
+  sourceValue,
+  util,
+} from '@openfn/language-common';
---
+++ packages/openelis/src/Utils.js
@@ -0,0 +1,43 @@
+import { composeNextState } from '@openfn/language-common';
+import {
+  request as commonRequest,
+  makeBasicAuthHeader,
+  logResponse
+} from '@openfn/language-common/util';
+import nodepath from 'node:path';
+
+export const prepareNextState = (state, response) => {
+  const { body, ...responseWithoutBody } = response;
+
+  if (!state.references) {
+    state.references = [];
+  }
+
+  return {
+    ...composeNextState(state, response.body),
+    response: responseWithoutBody,
+  };
+};
+
+export const request = (configuration = {}, method, path, options) => {
+  const { baseUrl, username, password } = configuration;
+  let { body = {}, query = {}, headers = {}, parseAs = 'json' } = options;
+
+  if (username && password) {
+    headers = makeBasicAuthHeader(username, password);
+  }
+
+  const opts = {
+    parseAs,
+    baseUrl,
+    body,
+    query,
+    headers: {
+      'content-type': 'application/json',
+      ...headers,
+    },
+  };
+  const safePath = nodepath.join(`/api/OpenELIS-Global/rest/${path}`);
+
+  return commonRequest(method, safePath, opts).then(logResponse)
+};
