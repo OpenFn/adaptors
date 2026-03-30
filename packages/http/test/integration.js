5ac06530db0e95280f7fd7cae11259ea18673388
Joe Clark <jclark@openfn.org>
2026-03-30 16:39:46 +0100

    Add formdata integration tests (#1615)
    * http:rewrite integration tests to ues koa
    
    * add tests of posting formdata
    
    * add a skipped test
    
    * add test showing current behaviour

--- packages/http/test/integration.js
+++ packages/http/test/integration.js
@@ -1,52 +1,55 @@
 import { expect } from 'chai';
-import http from 'http';
 import https from 'https';
+import Koa from 'koa';
+import Router from '@koa/router';
+import { koaBody } from 'koa-body';
 
 import { execute, get, post } from '../src/index.js';
 
 import { ca, key, cert } from './helpers/certs.js';
 
-const port = 8080;
-const httpServer = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/redirect':
-      res.writeHead(301, { Location: `http://localhost:${port}/new-location` });
-      res.end();
-      break;
-    case '/new-location':
-      res.writeHead(302, {
-        Location: `http://localhost:${port}/new-location-1`,
-      });
-      res.end();
-      break;
-    case '/new-location-1':
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ ok: true }));
-      break;
-    default:
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello, World!');
-      break;
-  }
+const router = new Router();
+router.get('/redirect', ctx => {
+  ctx.status = 301;
+  ctx.redirect(`http://localhost:${httpServer.address().port}/new-location`);
 });
+router.get('/new-location', ctx => {
+  ctx.redirect(`http://localhost:${httpServer.address().port}/new-location-1`);
+});
+router.get('/new-location-1', ctx => {
+  ctx.body = { ok: true };
+});
+router.post(
+  '/form',
+  koaBody({ multipart: true, urlencoded: false, json: false, text: false }),
+  ctx => {
+    if (!ctx.is('multipart/form-data')) {
+      ctx.status = 500;
+      return;
+    }
+    ctx.body = ctx.request.body;
+  },
+);
+router.all('(.*)', ctx => {
+  ctx.type = 'text/plain';
+  ctx.body = 'Hello, World!';
+});
+
+const app = new Koa();
+app.use(router.routes());
+const httpServer = app.listen(8080);
 
 // Create an HTTPS server for handling the redirected request
-const httpsPort = 1443;
-const certOptions = { key, cert };
-const httpsServer = https.createServer(certOptions || {}, (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, HTTPS World!');
+const httpsApp = new Koa();
+httpsApp.use(async ctx => {
+  ctx.type = 'text/plain';
+  ctx.body = 'Hello, HTTPS World!';
 });
+const httpsServer = https.createServer({ key, cert }, httpsApp.callback());
 
 describe('Integration tests', () => {
-  before(() => {
-    httpServer.listen(port, () => {
-      console.log(`HTTP server is running on http://localhost:${port}/`);
-    });
-
-    httpsServer.listen(httpsPort, () => {
-      console.log(`HTTPS server is running on https://localhost:${httpsPort}/`);
-    });
+  before(done => {
+    httpsServer.listen(1443, done);
   });
 
   it('can make a GET request', async () => {
@@ -75,6 +78,62 @@ describe('Integration tests', () => {
     expect(response.method).to.eql('POST');
   });
 
+  it('should post formdata with a contenttype and return json', async () => {
+    const state = {
+      configuration: {
+        baseUrl: `http://localhost:${httpServer.address().port}`,
+      },
+      data: {},
+    };
+    const { data, response } = await execute(
+      post('/form', { name: 'Joe', age: '30' }, { contentType: 'form' }),
+    )(state);
+
+    expect(response.statusCode).to.eql(200);
+    expect(data).to.eql({ name: 'Joe', age: '30' });
+  });
+
+  // This test exists because right now, users must pass the FormData content type
+  // Maybe later, if we expose FormData to job code, we can do something better
+  it('should fail when posting a raw FormData object without a content type', async () => {
+    const form = new FormData();
+    form.append('name', 'Joe');
+
+    const state = {
+      configuration: {
+        baseUrl: `http://localhost:${httpServer.address().port}`,
+      },
+      data: {},
+    };
+
+    const error = await execute(post('/form', form))(state).catch(e => e);
+    expect(error.statusCode).to.eql(500);
+  });
+
+  it('should return 500 when posting an empty body to /form', async () => {
+    const state = {
+      configuration: {
+        baseUrl: `http://localhost:${httpServer.address().port}`,
+      },
+      data: {},
+    };
+    const error = await execute(post('/form', null))(state).catch(e => e);
+    expect(error.statusCode).to.eql(500);
+  });
+
+  it('should return 500 when posting json to /form', async () => {
+    const state = {
+      configuration: {
+        baseUrl: `http://localhost:${httpServer.address().port}`,
+      },
+      data: {},
+    };
+    const error = await execute(post('/form', { name: 'Joe' }))(state).catch(
+      e => e,
+    );
+    expect(error.statusCode).to.eql(500);
+  });
+
   it('can follow redirects', async () => {
     const state = {
       configuration: {
@@ -86,7 +145,7 @@ describe('Integration tests', () => {
     const { data } = await execute(
       get('/redirect', {
         headers: { followAllRedirects: true },
-      })
+      }),
     )(state);
 
     expect(data).to.eql({ ok: true });
@@ -108,7 +167,7 @@ describe('Integration tests', () => {
           requestCert: false,
           rejectUnauthorized: true,
         },
-      })
+      }),
     )(state);
 
     expect(data).to.eql('Hello, HTTPS World!');
