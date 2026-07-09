9de5f1ffd9d8e1d8a5da414500c2298932cdb8e9
IZO-Ong <ong.izo.zh@gmail.com>
2026-07-09 20:36:33 +0800

    Implement client assertion signing and token exchange

--- packages/fayda/src/Utils.js
+++ packages/fayda/src/Utils.js
@@ -1,10 +1,93 @@
 import { composeNextState } from '@openfn/language-common';
 import {
   request as commonRequest,
-  makeBasicAuthHeader,
-  assertRelativeUrl,
+  logResponse,
 } from '@openfn/language-common/util';
-import nodepath from 'node:path';
+import crypto from 'node:crypto';
+
+const CLIENT_ASSERTION_TYPE =
+  'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
+
+const base64url = input => Buffer.from(input).toString('base64url');
+
+// Signs a private_key_jwt client assertion for eSignet's token endpoint,
+// per RFC 7523. configuration.privateKey is a base64-encoded JWK.
+export const generateClientAssertion = configuration => {
+  const { clientId, privateKey, tokenEndpoint } = configuration;
+
+  const jwk = JSON.parse(Buffer.from(privateKey, 'base64').toString('utf8'));
+  const keyObject = crypto.createPrivateKey({ key: jwk, format: 'jwk' });
+
+  const now = Math.floor(Date.now() / 1000);
+
+  const header = { alg: 'RS256', typ: 'JWT' };
+  const claims = {
+    iss: clientId,
+    sub: clientId,
+    aud: tokenEndpoint,
+    iat: now,
+    exp: now + 5 * 60,
+    jti: crypto.randomUUID(),
+  };
+
+  const signingInput = `${base64url(JSON.stringify(header))}.${base64url(
+    JSON.stringify(claims),
+  )}`;
+
+  const signature = crypto
+    .createSign('RSA-SHA256')
+    .update(signingInput)
+    .sign(keyObject);
+
+  return `${signingInput}.${base64url(signature)}`;
+};
+
+// Decodes the claims out of a JWT without verifying its signature.
+export const decodeClaims = jwt => {
+  const [, payload] = jwt.split('.');
+  return JSON.parse(Buffer.from(payload, 'base64url').toString('utf8'));
+};
+
+// Exchanges an authorization code for tokens by POSTing a signed
+// client assertion to eSignet's token endpoint.
+export const getToken = async (configuration, code) => {
+  const { clientId, redirectUri, tokenEndpoint } = configuration;
+
+  const clientAssertion = generateClientAssertion(configuration);
+
+  const body = new URLSearchParams();
+  body.append('grant_type', 'authorization_code');
+  body.append('code', code);
+  body.append('client_id', clientId);
+  body.append('redirect_uri', redirectUri);
+  body.append('client_assertion_type', CLIENT_ASSERTION_TYPE);
+  body.append('client_assertion', clientAssertion);
+
+  const options = {
+    body: body.toString(),
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    parseAs: 'json',
+  };
+
+  return commonRequest('POST', tokenEndpoint, options).then(logResponse);
+};
+
+// Attaches the configured access_token to a request against an
+// eSignet URL (eg. userInfoEndpoint).
+export const request = (configuration = {}, method, url, options = {}) => {
+  const { access_token } = configuration;
+
+  const headers = {
+    ...options.headers,
+    Authorization: `Bearer ${access_token}`,
+  };
+
+  return commonRequest(method, url, {
+    ...options,
+    headers,
+    parseAs: options.parseAs || 'json',
+  }).then(logResponse);
+};
 
 export const prepareNextState = (state, response) => {
   const { body, ...responseWithoutBody } = response;
@@ -14,55 +97,7 @@ export const prepareNextState = (state, response) => {
   }
 
   return {
-    ...composeNextState(state, response.body),
+    ...composeNextState(state, body),
     response: responseWithoutBody,
   };
-};
-
-// This helper function will call out to the backend service
-// and add authorisation headers
-// Refer to the common request function for options and details
-export const request = (configuration = {}, method, path, options) => {
-  // You might want to check that the path is not an absolute URL before
-  // appending credentials commonRequest will do this for you if you
-  // pass a baseURL to it and you don't need to build a path here
-  // assertRelativeUrl(path);
-
-  // TODO This example adds basic auth from config data
-  //       you may need to support other auth strategies
-  const { baseUrl, username, password } = configuration;
-  const headers = makeBasicAuthHeader(username, password);
-
-  // TODO You can define custom error messages here
-  //      The request function will throw if it receives
-  //      an error code (<=400), terminating the workflow
-  const errors = {
-    404: 'Page not found',
-  };
-
-  const opts = {
-    // Force the response to be parsed as JSON
-    parseAs: 'json',
-
-    // Include the error map
-    errors,
-
-    // Set the baseUrl from the config object
-    baseUrl,
-
-    ...options,
-
-    // You can add extra headers here if you want to
-    headers: {
-      'content-type': 'application/json',
-      ...headers,
-    },
-  };
-
-  // TODO you may want to add a prefix to the path
-  // use path.join to build the path safely
-  const safePath = nodepath.join(path);
-
-  // Make the actual request
-  return commonRequest(method, safePath, opts);
 };
