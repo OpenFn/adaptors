9183282b5a156c5187fb0c698f72da4edd30f462
Hunter Achieng <achienghunter@gmail.com>
2026-07-30 21:45:49 +0300

    feat: implement santempi adaptor
    Signed-off-by: Hunter Achieng <achienghunter@gmail.com>

---
+++ packages/santempi/CHANGELOG.md
@@ -0,0 +1,5 @@
+# @openfn/language-santempi
+
+## 1.0.0
+
+Implemented `http.get()`,`http.post()` and `http.request()` for santeMPI adaptor
---
+++ packages/santempi/src/Utils.js
@@ -0,0 +1,82 @@
+import { composeNextState } from '@openfn/language-common';
+import {
+  request as commonRequest,
+  logResponse,
+} from '@openfn/language-common/util';
+import nodepath from 'node:path';
+
+export const prepareNextState = (state, response) => {
+  const { body, ...responseWithoutBody } = response;
+
+  if (!state.references) {
+    state.references = [];
+  }
+
+  return {
+    ...composeNextState(state, response.body),
+    response: responseWithoutBody,
+  };
+};
+
+export const authorize = state => {
+  const { baseUrl, access_token, username, password, clientId, clientSecret } =
+    state.configuration;
+
+  if (access_token) {
+    console.log('Logging in with access token');
+    return state;
+  }
+  if (clientId && clientSecret && username && password && baseUrl) {
+    const formBody = new URLSearchParams();
+    formBody.append('username', username);
+    formBody.append('password', password);
+    formBody.append('grant_type', 'password');
+    formBody.append('client_id', clientId);
+    formBody.append('client_secret', clientSecret);
+
+    const options = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: 'POST',
+      parseAs: 'json',
+      body: formBody.toString(),
+      baseUrl,
+    };
+    return commonRequest('POST', '/auth/oauth2_token', options).then(
+      response => ({
+        ...state,
+        configuration: {
+          ...state.configuration,
+          access_token: response.body.access_token,
+          token_type: response.body.token_type,
+          expires_in: response.body.expires_in,
+        },
+      }),
+    );
+  } else {
+    throw new Error(
+      'Invalid authorization credentials. Include clientId, username, and password in state.configuration',
+    );
+  }
+};
+
+export const request = (configuration = {}, method, path, options) => {
+  const { baseUrl, access_token } = configuration;
+    if (options.query) console.log(`with params: `, options.query);
+
+  const opts = {
+    parseAs: 'json',
+    baseUrl,
+
+    ...options,
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+      ...options?.headers,
+    },
+  };
+
+  const safePath = nodepath.join(path);
+  return commonRequest(method, safePath, opts).then(logResponse);
+};
