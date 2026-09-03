96c682208a9acf631eef6259ec1843d98449958a
Joe Clark <joe@openfn.org>
2026-09-03 13:52:22 +0100

    fhir-4: support tls object on config

--- packages/fhir-4/test/util.test.ts
+++ packages/fhir-4/test/util.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { sortBundle, logValidationErrors } from '../src/util';
+import { sortBundle, logValidationErrors, getTLSOptions } from '../src/util';
 
 const mockLogger = { log: () => {}, error: () => {} };
 
@@ -202,5 +202,34 @@ describe('logValidationErrors', () => {
         },
       },
     });
+  });
+});
+
+describe('getTLSOptions', () => {
+  it('prefers requestOptions.tls over configuration.tls', () => {
+    const configuration = {
+      tls: { ca: 'config-ca', cert: 'config-cert' },
+    };
+
+    const requestOptions = {
+      tls: { ca: 'request-ca', cert: 'request-cert' },
+    };
+
+    const result = getTLSOptions(configuration, requestOptions);
+    expect(result).to.deep.equal(requestOptions.tls);
+  });
+
+  it('falls back to configuration.tls if requestOptions.tls is not provided', () => {
+    const configuration = {
+      tls: { ca: 'config-ca', cert: 'config-cert' },
+    };
+
+    const result = getTLSOptions(configuration, {});
+    expect(result).to.deep.equal(configuration.tls);
+  });
+
+  it('returns undefined if no TLS config is found', () => {
+    const result = getTLSOptions({}, {});
+    expect(result).to.be.undefined;
   });
 });
